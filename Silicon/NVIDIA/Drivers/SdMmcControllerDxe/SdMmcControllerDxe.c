d09f9c635c577b4f7b436a448a99b167738198e7
Jeff Brasen <jbrasen@nvidia.com>
2018-10-26 18:00:04 -0600

    Silicon/NVIDIA/ScMmcController: Update to DeviceDiscoveryDriverLib
    Update implementation to use device discovery driver library interface.
    Also, move set frequency call to device initialization phase.
    
    Change-Id: I45e79b327ca8247b3ad15f7fcde708c2d5eab72c
    Reviewed-on: https://git-master.nvidia.com/r/1936893
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>
    Tested-by: Jeff Brasen <jbrasen@nvidia.com>

--- Silicon/NVIDIA/Drivers/SdMmcControllerDxe/SdMmcControllerDxe.c
+++ Silicon/NVIDIA/Drivers/SdMmcControllerDxe/SdMmcControllerDxe.c
@@ -21,14 +21,8 @@
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiLib.h>
 #include <Library/IoLib.h>
-#include <libfdt.h>
-
+#include <Library/DeviceDiscoveryDriverLib.h>
 #include <Protocol/SdMmcOverride.h>
-#include <Protocol/NonDiscoverableDevice.h>
-#include <Protocol/DeviceTreeCompatibility.h>
-#include <Protocol/ClockNodeProtocol.h>
-#include <Protocol/ResetNodeProtocol.h>
-#include <Protocol/ArmScmiClockProtocol.h>
 
 #include "SdMmcControllerPrivate.h"
 
@@ -54,8 +48,6 @@ SdMmcCapability (
 {
   SD_MMC_HC_SLOT_CAP  *Capability = (SD_MMC_HC_SLOT_CAP *)SdMmcHcSlotCapability;
   EFI_STATUS          Status;
-  SCMI_CLOCK_PROTOCOL *ClockProtocol = NULL;
-  NVIDIA_CLOCK_NODE_PROTOCOL *ClockNodeProtocol = NULL;
   UINT64              Rate;
 
 
@@ -63,22 +55,7 @@ SdMmcCapability (
     return EFI_INVALID_PARAMETER;
   }
 
-  Status = gBS->LocateProtocol (&gArmScmiClockProtocolGuid, NULL, (VOID **)&ClockProtocol);
-  if (EFI_ERROR (Status)) {
-    return Status;
-  }
-
-  Status = gBS->HandleProtocol (ControllerHandle, &gNVIDIAClockNodeProtocolGuid, (VOID **)&ClockNodeProtocol);
-  if (EFI_ERROR (Status)) {
-    return Status;
-  }
-
-  Status = ClockProtocol->RateSet (ClockProtocol, ClockNodeProtocol->ClockEntries[0].ClockId, SD_MMC_MAX_CLOCK);
-  if (!EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "%a failed to set clock rate %r\r\n", __FUNCTION__, Status));
-  }
-
-  Status = ClockProtocol->RateGet (ClockProtocol, ClockNodeProtocol->ClockEntries[0].ClockId, &Rate);
+  Status = DeviceDiscoveryGetClockFreq (ControllerHandle, "sdmmc", &Rate);
   if (!EFI_ERROR (Status)) {
     if (Rate > SD_MMC_MAX_CLOCK) {
       DEBUG ((EFI_D_ERROR, "%a: Clock rate %llu out of range for SDHCI\r\n",__FUNCTION__,Rate));
@@ -100,7 +77,7 @@ SdMmcCapability (
   @param[in]      ControllerHandle      The EFI_HANDLE of the controller.
   @param[in]      Slot                  The 0 based slot index.
   @param[in]      PhaseType             The type of operation and whether the
-                                        hook is invoked right before (pre) or
+                     Ok, the access denied is due to the memory already being                    hook is invoked right before (pre) or
                                         right after (post)
 
   @retval EFI_SUCCESS           The override function completed successfully.
@@ -115,36 +92,12 @@ SdMmcNotify (
   IN      EDKII_SD_MMC_PHASE_TYPE         PhaseType
   )
 {
-  UINT64                              SlotBaseAddress  = 0;
+  EFI_PHYSICAL_ADDRESS SlotBaseAddress  = 0;
+  UINTN                SlotSize;
+  EFI_STATUS           Status;
 
-  NON_DISCOVERABLE_DEVICE             *Device;
-  EFI_STATUS                          Status;
-  EFI_ACPI_ADDRESS_SPACE_DESCRIPTOR   *Desc;
-  UINT8                               CurrentResource = 0;
-
-  Status = gBS->HandleProtocol (ControllerHandle,
-                  &gEdkiiNonDiscoverableDeviceProtocolGuid, (VOID **)&Device);
+  Status = DeviceDiscoveryGetMmioRegion (ControllerHandle, Slot, &SlotBaseAddress, &SlotSize);
   if (EFI_ERROR (Status)) {
-    return Status;
-  }
-
-  //
-  // We only support MMIO devices, so iterate over the resources to ensure
-  // that they only describe things that we can handle
-  //
-  for (Desc = Device->Resources; Desc->Desc != ACPI_END_TAG_DESCRIPTOR;
-       Desc = (VOID *)((UINT8 *)Desc + Desc->Len + 3)) {
-    if (Desc->Desc != ACPI_ADDRESS_SPACE_DESCRIPTOR ||
-        Desc->ResType != ACPI_ADDRESS_SPACE_TYPE_MEM) {
-      break;
-    }
-    if (CurrentResource == Slot) {
-      SlotBaseAddress = Desc->AddrRangeMin;
-      break;
-    }
-  }
-
-  if (SlotBaseAddress == 0) {
     DEBUG ((DEBUG_ERROR, "SdMmcNotify: Unable to locate address range for slot %d\n", Slot));
     return EFI_UNSUPPORTED;
   }
@@ -164,312 +117,58 @@ EDKII_SD_MMC_OVERRIDE gSdMmcOverride = {
   SdMmcNotify
 };
 
-/**
-  This is function is caused to allow the system to check if this implementation supports
-  the device tree node. If EFI_SUCCESS is returned then handle will be created and driver binding
-  will occur.
+NVIDIA_COMPATIBILITY_MAPPING gDeviceCompatibilityMap[] = {
+    { "nvidia,tegra186-sdhci", &gEdkiiNonDiscoverableSdhciDeviceGuid },
+    { "nvidia,tegra194-sdhci", &gEdkiiNonDiscoverableSdhciDeviceGuid },
+    { NULL, NULL }
+};
 
-  @param[in]  This                   The instance of the NVIDIA_DEVICE_TREE_BINDING_PROTOCOL.
-  @param[in]  Node                   The pointer to the requested node info structure.
-  @param[out] DeviceType             Pointer to allow the return of the device type
-  @param[out] PciIoInitialize        Pointer to allow return of function that will be called
-                                       when the PciIo subsystem connects to this device.
-                                       Note that this will may not be called if the device
-                                       is not in the boot path.
-
-  @return EFI_SUCCESS               The node is supported by this instance
-  @return EFI_UNSUPPORTED           The node is not supported by this instance
-**/
-EFI_STATUS
-DeviceTreeIsSupported (
-  IN NVIDIA_DEVICE_TREE_COMPATIBILITY_PROTOCOL   *This,
-  IN CONST NVIDIA_DEVICE_TREE_NODE_PROTOCOL      *Node,
-  OUT EFI_GUID                                   **DeviceType,
-  OUT NON_DISCOVERABLE_DEVICE_INIT               *PciIoInitialize
-  )
-{
-
-  if ((Node == NULL) ||
-      (DeviceType == NULL) ||
-      (PciIoInitialize == NULL)) {
-    return EFI_INVALID_PARAMETER;
-  }
-
-  if ((0 != fdt_node_check_compatible (Node->DeviceTreeBase, Node->NodeOffset, "nvidia,tegra186-sdhci")) &&
-      (0 != fdt_node_check_compatible (Node->DeviceTreeBase, Node->NodeOffset, "nvidia,tegra194-sdhci"))) {
-    return EFI_UNSUPPORTED;
-  }
-
-  *DeviceType = &gEdkiiNonDiscoverableSdhciDeviceGuid;
-  *PciIoInitialize = NULL;
-
-  return EFI_SUCCESS;
-}
-
-NVIDIA_DEVICE_TREE_COMPATIBILITY_PROTOCOL gDeviceTreeCompatibilty = {
-    DeviceTreeIsSupported
+NVIDIA_DEVICE_DISCOVERY_CONFIG gDeviceDiscoverDriverConfig = {
+    .DriverName = L"NVIDIA SdMmc controller driver",
+    .UseDriverBinding = TRUE,
+    .AutoEnableClocks = TRUE,
+    .AutoSetParents = TRUE,
+    .AutoDeassertReset = TRUE,
+    .SkipEdkiiNondiscoverableInstall = FALSE
 };
 
 /**
-  Supported function of Driver Binding protocol for this driver.
-  Test to see if this driver supports ControllerHandle.
+  Callback that will be invoked at various phases of the driver initialization
 
-  @param This                   Protocol instance pointer.
-  @param Controller             Handle of device to test.
-  @param RemainingDevicePath    A pointer to the device path.
-                                it should be ignored by device driver.
+  This function allows for modification of system behavior at various points in
+  the driver binding process.
 
-  @retval EFI_SUCCESS           This driver supports this device.
-  @retval EFI_ALREADY_STARTED   This driver is already running on this device.
-  @retval other                 This driver does not support this device.
+  @param[in] Phase                    Current phase of the driver initialization
+  @param[in] DriverHandle             Handle of the driver.
+  @param[in] ControllerHandle         Handle of the controller.
+  @param[in] DeviceTreeNode           Pointer to the device tree node protocol is available.
 
-**/
-EFI_STATUS
-EFIAPI
-SdMmcControllerSupported (
-  IN EFI_DRIVER_BINDING_PROTOCOL    *This,
-  IN EFI_HANDLE                     Controller,
-  IN EFI_DEVICE_PATH_PROTOCOL       *RemainingDevicePath
-  )
-{
-  EFI_STATUS            Status;
-  NON_DISCOVERABLE_DEVICE *NonDiscoverableProtocol = NULL;
-
-  //
-  // Attempt to open NonDiscoverable Protocol
-  //
-  Status = gBS->OpenProtocol (
-                  Controller,
-                  &gNVIDIANonDiscoverableDeviceProtocolGuid,
-                  (VOID **) &NonDiscoverableProtocol,
-                  This->DriverBindingHandle,
-                  Controller,
-                  EFI_OPEN_PROTOCOL_BY_DRIVER
-                  );
-  if (EFI_ERROR (Status)) {
-    return Status;
-  }
-
-  if (CompareGuid (NonDiscoverableProtocol->Type, &gEdkiiNonDiscoverableSdhciDeviceGuid)) {
-    Status = EFI_SUCCESS;
-  } else {
-    Status = EFI_UNSUPPORTED;
-  }
-
-  gBS->CloseProtocol (
-         Controller,
-         &gNVIDIANonDiscoverableDeviceProtocolGuid,
-         This->DriverBindingHandle,
-         Controller
-         );
-
-  return Status;
-
-}
-
-/**
-  This routine is called right after the .Supported() called and
-  Start this driver on ControllerHandle.
-
-  @param This                   Protocol instance pointer.
-  @param Controller             Handle of device to bind driver to.
-  @param RemainingDevicePath    A pointer to the device path.
-                                it should be ignored by device driver.
-
-  @retval EFI_SUCCESS           This driver is added to this device.
-  @retval EFI_ALREADY_STARTED   This driver is already running on this device.
-  @retval other                 Some error occurs when binding this driver to this device.
+  @retval EFI_SUCCESS              Operation successful.
+  @retval EFI_SUCCESS              Driver does not handle this phase
+  @retval others                   Error occurred
 
 **/
 EFI_STATUS
-EFIAPI
-SdMmcControllerStart (
-  IN EFI_DRIVER_BINDING_PROTOCOL    *This,
-  IN EFI_HANDLE                     Controller,
-  IN EFI_DEVICE_PATH_PROTOCOL       *RemainingDevicePath
+DeviceDiscoveryNotify (
+  IN  NVIDIA_DEVICE_DISCOVERY_PHASES         Phase,
+  IN  EFI_HANDLE                             DriverHandle,
+  IN  EFI_HANDLE                             ControllerHandle,
+  IN  CONST NVIDIA_DEVICE_TREE_NODE_PROTOCOL *DeviceTreeNode OPTIONAL
   )
 {
-  EFI_STATUS                        Status;
-  NON_DISCOVERABLE_DEVICE           *NonDiscoverableProtocol = NULL;
-  NVIDIA_CLOCK_NODE_PROTOCOL        *ClockProtocol = NULL;
-  NVIDIA_RESET_NODE_PROTOCOL        *ResetProtocol = NULL;
-  //
-  // Attempt to open NonDiscoverable Protocol
-  //
-  Status = gBS->OpenProtocol (
-                  Controller,
-                  &gNVIDIANonDiscoverableDeviceProtocolGuid,
-                  (VOID **) &NonDiscoverableProtocol,
-                  This->DriverBindingHandle,
-                  Controller,
-                  EFI_OPEN_PROTOCOL_BY_DRIVER
-                  );
-
-  if (!CompareGuid (NonDiscoverableProtocol->Type, &gEdkiiNonDiscoverableSdhciDeviceGuid)) {
-    Status = EFI_UNSUPPORTED;
-    goto ErrorExit;
-  }
-
-  Status = gBS->HandleProtocol (Controller, &gNVIDIAClockNodeProtocolGuid, (VOID **)&ClockProtocol);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "%a, no clock node protocol\r\n",__FUNCTION__));
-    goto ErrorExit;
-  }
-  Status = ClockProtocol->EnableAll (ClockProtocol, TRUE);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "%a, failed to enable clocks %r\r\n",__FUNCTION__,Status));
-    goto ErrorExit;
-  }
-
-  Status = gBS->HandleProtocol (Controller, &gNVIDIAResetNodeProtocolGuid, (VOID **)&ResetProtocol);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "%a, no reset node protocol\r\n",__FUNCTION__));
-    goto ErrorExit;
-  }
-  Status = ResetProtocol->DeassertAll (ResetProtocol);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "%a, failed to deassert resets %r\r\n",__FUNCTION__,Status));
-    goto ErrorExit;
-  }
-
-  Status = gBS->InstallMultipleProtocolInterfaces (
-                  &Controller,
-                  &gEdkiiNonDiscoverableDeviceProtocolGuid,
-                  NonDiscoverableProtocol,
-                  NULL
-                  );
-ErrorExit:
-  if (EFI_ERROR (Status)) {
-
-    gBS->CloseProtocol (
-          Controller,
-          &gNVIDIANonDiscoverableDeviceProtocolGuid,
-          This->DriverBindingHandle,
-          Controller
-          );
-  }
-
-  return Status;
-}
-
-/**
-  Stop this driver on ControllerHandle.
-
-  @param This               Protocol instance pointer.
-  @param Controller         Handle of device to stop driver on.
-  @param NumberOfChildren   Not used.
-  @param ChildHandleBuffer  Not used.
-
-  @retval EFI_SUCCESS   This driver is removed from this device.
-  @retval other         Some error occurs when removing this driver from this device.
-
-**/
-EFI_STATUS
-EFIAPI
-SdMmcControllerStop (
-  IN EFI_DRIVER_BINDING_PROTOCOL    *This,
-  IN EFI_HANDLE                     Controller,
-  IN UINTN                          NumberOfChildren,
-  IN EFI_HANDLE                     *ChildHandleBuffer
-  )
-{
-  EFI_STATUS                        Status;
-  NON_DISCOVERABLE_DEVICE           *NonDiscoverableProtocol = NULL;
-
-  //
-  // Attempt to open NonDiscoverable Protocol
-  //
-  Status = gBS->OpenProtocol (
-                  Controller,
-                  &gNVIDIANonDiscoverableDeviceProtocolGuid,
-                  (VOID **) &NonDiscoverableProtocol,
-                  This->DriverBindingHandle,
-                  Controller,
-                  EFI_OPEN_PROTOCOL_GET_PROTOCOL
-                  );
-  if (EFI_ERROR (Status)) {
-    return EFI_DEVICE_ERROR;
-  }
-
-  if (CompareGuid (NonDiscoverableProtocol->Type, &gEdkiiNonDiscoverableSdhciDeviceGuid)) {
-    Status = gBS->UninstallMultipleProtocolInterfaces (
-                    This->DriverBindingHandle,
-                    &gEdkiiNonDiscoverableDeviceProtocolGuid,
-                    NonDiscoverableProtocol,
-                    NULL
-                    );
-  } else {
-    Status = EFI_UNSUPPORTED;
-  }
-
-  if (EFI_ERROR (Status)) {
-    return EFI_DEVICE_ERROR;
-  }
-
-  //
-  // Close protocols opened by SdMmc Controller driver
-  //
-  return gBS->CloseProtocol (
-                Controller,
-                &gNVIDIANonDiscoverableDeviceProtocolGuid,
-                This->DriverBindingHandle,
-                Controller
-                );
-}
-
-///
-/// EFI_DRIVER_BINDING_PROTOCOL instance
-///
-EFI_DRIVER_BINDING_PROTOCOL mDriverBindingProtocol = {
-  SdMmcControllerSupported,
-  SdMmcControllerStart,
-  SdMmcControllerStop,
-  0x0,
-  NULL,
-  NULL
-};
-
-/**
-  Initialize the SD MMC Controller Protocol Driver
-
-  @param  ImageHandle   of the loaded driver
-  @param  SystemTable   Pointer to the System Table
-
-  @retval EFI_SUCCESS           Protocol registered
-  @retval EFI_OUT_OF_RESOURCES  Cannot allocate protocol data structure
-  @retval EFI_DEVICE_ERROR      Hardware problems
-
-**/
-EFI_STATUS
-SdMmcControllerInitialize (
-  IN EFI_HANDLE         ImageHandle,
-  IN EFI_SYSTEM_TABLE   *SystemTable
-  )
-{
-  EFI_STATUS  Status;
-
-  Status = EfiLibInstallDriverBinding (
-             ImageHandle,
-             SystemTable,
-             &mDriverBindingProtocol,
-             ImageHandle);
-
-  if (EFI_ERROR (Status)) {
-    DEBUG ((EFI_D_ERROR, "%a: Failed to install driver binding protocol: %r\r\n",__FUNCTION__,Status));
-    return Status;
-  }
-
-  Status = gBS->InstallMultipleProtocolInterfaces (
-                  &mDriverBindingProtocol.DriverBindingHandle,
+  switch (Phase) {
+  case DeviceDiscoveryDriverStart:
+    return gBS->InstallMultipleProtocolInterfaces (
+                  &DriverHandle,
                   &gEdkiiSdMmcOverrideProtocolGuid,
                   &gSdMmcOverride,
-                  &gNVIDIADeviceTreeCompatibilityProtocolGuid,
-                  &gDeviceTreeCompatibilty,
                   NULL
                   );
-  ASSERT_EFI_ERROR (Status);
 
-  return Status;
+  case DeviceDiscoveryDriverBindingStart:
+    return DeviceDiscoverySetClockFreq (ControllerHandle, "sdmmc", SD_MMC_MAX_CLOCK);
+
+  default:
+    return EFI_SUCCESS;
+  }
 }
-
-
