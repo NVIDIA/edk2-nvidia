0a53d20bb5b68d05b81583971c9440286fa56024
Jeff Brasen <jbrasen@nvidia.com>
2019-06-19 11:11:16 -0600

    Silicon/NVIDIA: Move Hob list to largest region
    Move the Hob list to hardest region when memory is detected to allow for
    larger space. This is needed for code coverage enabled builds as the
    build size is larger and won't fit in the main region.
    
    Change-Id: I43499f22be36db71ea2d7b23ecf3df416b9bb002
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/edk2-platforms/+/2139674
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Jeff Brasen <jbrasen@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- Silicon/NVIDIA/Library/DramCarveoutLib/DramCarveoutLib.c
+++ Silicon/NVIDIA/Library/DramCarveoutLib/DramCarveoutLib.c
@@ -19,6 +19,89 @@
 #include <Library/HobLib.h>
 #include <Pi/PiHob.h>
 #include <Library/SortLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/PrePiHobListPointerLib.h>
+
+/**
+  Migrate Hob list to the new region.
+
+  @param[in] RegionStart              The region to migrate the HOB list to.
+  @param[in] RegionSize               Region size of the new region.
+
+  @retval EFI_SUCCESS                 Hob list moved.
+  @return others                      Other error.
+**/
+STATIC
+EFI_STATUS
+MigrateHobList (
+  IN EFI_PHYSICAL_ADDRESS  RegionStart,
+  IN UINTN                 RegionSize
+)
+{
+  EFI_HOB_HANDOFF_INFO_TABLE  *OldHob;
+  EFI_PHYSICAL_ADDRESS        OldHobAddress;
+  EFI_PHYSICAL_ADDRESS        RegionEndAddress;
+  EFI_HOB_HANDOFF_INFO_TABLE  *NewHob;
+  EFI_HOB_MEMORY_ALLOCATION   *Allocation;
+
+  //Keep Hob list on one side of 32-bit
+  if ((RegionStart < MAX_UINT32) && ((RegionStart + RegionSize) > MAX_UINT32)) {
+    RegionSize = ((UINTN)MAX_UINT32) + 1 - RegionStart;
+  }
+
+  OldHob = (EFI_HOB_HANDOFF_INFO_TABLE *)PrePeiGetHobList ();
+  OldHobAddress = (EFI_PHYSICAL_ADDRESS)OldHob;
+
+  ASSERT (OldHob->EfiFreeMemoryBottom > OldHobAddress);
+  ASSERT (OldHob->EfiFreeMemoryTop >= OldHob->EfiFreeMemoryBottom);
+  ASSERT (OldHob->EfiEndOfHobList > OldHobAddress);
+
+  if ((OldHobAddress >= RegionStart) &&
+      (OldHobAddress < (RegionStart + RegionSize))) {
+    //Hob list is already in the correct region, check if memory at end of region is larger
+    if ((RegionStart + RegionSize - OldHob->EfiMemoryTop) > (OldHob->EfiFreeMemoryTop - OldHobAddress)) {
+      RegionSize = RegionStart + RegionSize - OldHob->EfiMemoryTop;
+      RegionStart = OldHob->EfiMemoryTop;
+    } else {
+      //Free area is smaller then current, do not move
+      return EFI_SUCCESS;
+    }
+  }
+
+  RegionEndAddress = RegionStart + RegionSize;
+  //Filter out any prior allocations
+  Allocation = (EFI_HOB_MEMORY_ALLOCATION *)(VOID *)OldHob;
+  while ((Allocation = (EFI_HOB_MEMORY_ALLOCATION *)GetNextHob (EFI_HOB_TYPE_MEMORY_ALLOCATION, GET_NEXT_HOB (Allocation))) != NULL) {
+    if ((Allocation->AllocDescriptor.MemoryBaseAddress >= RegionStart) &&
+        (Allocation->AllocDescriptor.MemoryBaseAddress < (RegionStart + RegionSize))) {
+      EFI_PHYSICAL_ADDRESS EndAddress = Allocation->AllocDescriptor.MemoryBaseAddress + Allocation->AllocDescriptor.MemoryLength;
+      if ((Allocation->AllocDescriptor.MemoryBaseAddress - RegionStart) > (RegionEndAddress - EndAddress)) {
+        RegionSize = Allocation->AllocDescriptor.MemoryBaseAddress - RegionStart;
+        RegionEndAddress = RegionStart + RegionSize;
+      } else {
+        RegionStart = EndAddress;
+        RegionSize = RegionEndAddress - EndAddress;
+      }
+    }
+  }
+
+  if ((RegionStart + RegionSize - OldHob->EfiMemoryTop) <= (OldHob->EfiFreeMemoryTop - OldHobAddress)) {
+    //Free area is smaller then current, do not move
+    return EFI_SUCCESS;
+  }
+
+  //Move Hob list to take full region
+  NewHob = (VOID *)RegionStart;
+  CopyMem (NewHob, OldHob, OldHob->EfiFreeMemoryBottom - OldHobAddress);
+  NewHob->EfiEndOfHobList = RegionStart + OldHob->EfiEndOfHobList - OldHobAddress;
+  NewHob->EfiFreeMemoryBottom = RegionStart + OldHob->EfiFreeMemoryBottom - OldHobAddress;
+  NewHob->EfiFreeMemoryTop = RegionStart + RegionSize;
+  NewHob->EfiMemoryBottom = RegionStart;
+  NewHob->EfiMemoryTop = RegionStart + RegionSize;
+
+  PrePeiSetHobList ((VOID *)NewHob);
+  return EFI_SUCCESS;
+}
 
 
 /**
@@ -79,6 +162,8 @@ InstallDramWithCarveouts (
   UINTN DramIndex = 0;
   UINTN CarveoutIndex = 0;
   UINTN InstalledRegions = 0;
+  EFI_PHYSICAL_ADDRESS LargestRegionStart = 0;
+  UINTN MaxSize = 0;
   EFI_RESOURCE_ATTRIBUTE_TYPE  ResourceAttributes;
 
   PerformQuickSort (
@@ -129,12 +214,17 @@ InstallDramWithCarveouts (
     //No more carveouts or carveout is after dram region
     if ((CarveoutRegionsCount == CarveoutIndex) ||
         ((DramRegions[DramIndex].MemoryBaseAddress + DramRegions[DramIndex].MemoryLength) <= CarveoutRegions[CarveoutIndex].MemoryBaseAddress)) {
+      DEBUG ((DEBUG_ERROR, "Region Dram: %016x, %016x\r\n", DramRegions[DramIndex].MemoryBaseAddress, DramRegions[DramIndex].MemoryLength));
       BuildResourceDescriptorHob (
         EFI_RESOURCE_SYSTEM_MEMORY,
         ResourceAttributes,
         DramRegions[DramIndex].MemoryBaseAddress,
         DramRegions[DramIndex].MemoryLength
       );
+      if (DramRegions[DramIndex].MemoryLength > MaxSize) {
+        LargestRegionStart = DramRegions[DramIndex].MemoryBaseAddress;
+        MaxSize = DramRegions[DramIndex].MemoryLength;
+      }
 
       DramIndex++;
       InstalledRegions++;
@@ -142,12 +232,18 @@ InstallDramWithCarveouts (
       UINTN CarveoutEnd = CarveoutRegions[CarveoutIndex].MemoryBaseAddress + CarveoutRegions[CarveoutIndex].MemoryLength;
       UINTN DramEnd = DramRegions[DramIndex].MemoryBaseAddress + DramRegions[DramIndex].MemoryLength;
       if (DramRegions[DramIndex].MemoryBaseAddress < CarveoutRegions[CarveoutIndex].MemoryBaseAddress) {
+        DEBUG ((DEBUG_ERROR, "Region: %016x, %016x\r\n", DramRegions[DramIndex].MemoryBaseAddress, CarveoutRegions[CarveoutIndex].MemoryBaseAddress - DramRegions[DramIndex].MemoryBaseAddress));
         BuildResourceDescriptorHob (
                 EFI_RESOURCE_SYSTEM_MEMORY,
                 ResourceAttributes,
                 DramRegions[DramIndex].MemoryBaseAddress,
                 CarveoutRegions[CarveoutIndex].MemoryBaseAddress - DramRegions[DramIndex].MemoryBaseAddress
               );
+        if ((CarveoutRegions[CarveoutIndex].MemoryBaseAddress - DramRegions[DramIndex].MemoryBaseAddress) > MaxSize) {
+          LargestRegionStart = DramRegions[DramIndex].MemoryBaseAddress;
+          MaxSize = CarveoutRegions[CarveoutIndex].MemoryBaseAddress - DramRegions[DramIndex].MemoryBaseAddress;
+        }
+
         InstalledRegions++;
       }
 
@@ -169,6 +265,7 @@ InstallDramWithCarveouts (
     }
   }
 
+  MigrateHobList (LargestRegionStart, MaxSize);
   *FinalRegionsCount = InstalledRegions;
   return EFI_SUCCESS;
 }
