9745baad5caf09de5af813b6ef1fc5030c898824
Jeff Brasen <jbrasen@nvidia.com>
2019-09-20 01:56:37 -0600

    Silicon/NVIDIA: Add support for kernel specific dtb
    Add support for loading a device tree from firmware storage for the
    kernel while keeping the ramloaded version for UEFI purposes.
    
    This uses the same triggers as emulated variables to determine if the
    kernel-dtb partition should be attempted to be loaded and will fall back
    to the cpubl change.
    
    Change-Id: I862d701f5e3329b2a709d8c461b0e3da36cc7eab
    Reviewed-on: https://git-master.nvidia.com/r/2202203
    Tested-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    GVS: Gerrit_Virtual_Submit
    Reviewed-by: Thierry Reding <treding@nvidia.com>
    Tested-by: Thierry Reding <treding@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

---
+++ Silicon/NVIDIA/Library/DxeDtPlatformDtbLoaderLib/DxeDtPlatformDtbKernelLoaderLib.c
@@ -0,0 +1,167 @@
+/** @file
+*
+*  Copyright (c) 2018-2019, NVIDIA CORPORATION. All rights reserved.
+*  Copyright (c) 2017, Linaro, Ltd. All rights reserved.
+*
+*  This program and the accompanying materials
+*  are licensed and made available under the terms and conditions of the BSD License
+*  which accompanies this distribution.  The full text of the license may be found at
+*  http://opensource.org/licenses/bsd-license.php
+*
+*  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+*
+**/
+
+#include <PiDxe.h>
+
+#include <Library/BaseLib.h>
+#include <Library/HobLib.h>
+#include <Library/DebugLib.h>
+#include <libfdt.h>
+#include "FloorSweepPrivate.h"
+
+#include <Library/UefiBootServicesTableLib.h>
+#include <Protocol/PartitionInfo.h>
+#include <Protocol/BlockIo.h>
+
+/**
+  Return a pool allocated copy of the DTB image that is appropriate for
+  booting the current platform via DT.
+
+  @param[out]   Dtb                   Pointer to the DTB copy
+  @param[out]   DtbSize               Size of the DTB copy
+
+  @retval       EFI_SUCCESS           Operation completed successfully
+  @retval       EFI_NOT_FOUND         No suitable DTB image could be located
+  @retval       EFI_OUT_OF_RESOURCES  No pool memory available
+
+**/
+EFI_STATUS
+EFIAPI
+DtPlatformLoadDtb (
+  OUT   VOID        **Dtb,
+  OUT   UINTN       *DtbSize
+  )
+{
+  EFI_STATUS                  Status;
+  UINTN                       NumOfHandles;
+  EFI_HANDLE                  *HandleBuffer = NULL;
+  UINT64                      Size;
+  EFI_PARTITION_INFO_PROTOCOL *PartitionInfo = NULL;
+  BOOLEAN                     ValidFlash = FALSE;
+  UINTN                       Index;
+  VOID                        *Hob = NULL;
+  EFI_BLOCK_IO_PROTOCOL       *BlockIo;
+
+
+  *Dtb = NULL;
+
+  if (!PcdGetBool(PcdEmuVariableNvModeEnable)) {
+    do {
+      Status = gBS->LocateHandleBuffer (ByProtocol,
+                                        &gEfiPartitionInfoProtocolGuid,
+                                        NULL,
+                                        &NumOfHandles,
+                                        &HandleBuffer);
+
+      if (EFI_ERROR(Status)) {
+        Status = EFI_UNSUPPORTED;
+        break;
+      }
+
+      for (Index = 0; Index < NumOfHandles; Index++) {
+        Status = gBS->HandleProtocol (HandleBuffer[Index],
+                                    &gEfiPartitionInfoProtocolGuid,
+                                    (VOID **)&PartitionInfo);
+
+        if (EFI_ERROR(Status) || (PartitionInfo == NULL)) {
+          continue;
+        }
+
+        if (PartitionInfo->Info.Gpt.StartingLBA > PartitionInfo->Info.Gpt.EndingLBA) {
+          continue;
+        }
+
+        if (PartitionInfo->Type != PARTITION_TYPE_GPT) {
+          continue;
+        }
+
+        if (0 == StrnCmp (PartitionInfo->Info.Gpt.PartitionName,
+                          PcdGetPtr(PcdKernelDtbPartitionName),
+                          StrnLenS(PcdGetPtr(PcdKernelDtbPartitionName), sizeof(PartitionInfo->Info.Gpt.PartitionName)))) {
+          break;
+        }
+      }
+
+      if (Index == NumOfHandles) {
+        break;
+      }
+
+      ValidFlash = TRUE;
+
+      Status = gBS->HandleProtocol (HandleBuffer[Index],
+                                  &gEfiBlockIoProtocolGuid,
+                                  (VOID **)&BlockIo);
+
+      if (EFI_ERROR(Status) || (BlockIo == NULL)) {
+        ValidFlash = FALSE;
+        break;
+      }
+
+      Size = MultU64x32 (BlockIo->Media->LastBlock+1, BlockIo->Media->BlockSize);
+
+      *Dtb = NULL;
+      Status = gBS->AllocatePool (EfiBootServicesData,
+                                  Size,
+                                  (VOID **)Dtb);
+
+      if (EFI_ERROR(Status) || (*Dtb == NULL)) {
+        ValidFlash = FALSE;
+        break;
+      }
+      Status = BlockIo->ReadBlocks (BlockIo,
+                                    BlockIo->Media->MediaId,
+                                    0,
+                                    Size,
+                                    *Dtb);
+      if (EFI_ERROR(Status)) {
+        ValidFlash = FALSE;
+        break;
+      }
+    } while (FALSE);
+  }
+
+  if (HandleBuffer != NULL) {
+    gBS->FreePool (HandleBuffer);
+    HandleBuffer = NULL;
+  }
+
+  if (!ValidFlash) {
+    if (*Dtb != NULL) {
+      gBS->FreePool (*Dtb);
+      *Dtb = NULL;
+    }
+    Hob = GetFirstGuidHob (&gFdtHobGuid);
+    if (Hob == NULL || GET_GUID_HOB_DATA_SIZE (Hob) != sizeof (UINT64)) {
+      return EFI_NOT_FOUND;
+    }
+    *Dtb = (VOID *)(UINTN)*(UINT64 *)GET_GUID_HOB_DATA (Hob);
+  }
+
+  if (fdt_check_header (*Dtb) != 0) {
+    if (ValidFlash) {
+      *Dtb += PcdGet32 (PcdBootImgSigningHeaderSize);
+    }
+    if (fdt_check_header (*Dtb) != 0) {
+      DEBUG ((DEBUG_ERROR, "%a: No DTB found @ 0x%p\n", __FUNCTION__,
+        *Dtb));
+      return EFI_NOT_FOUND;
+    }
+  }
+
+  UpdateCpuFloorsweepingConfig (*Dtb);
+  *DtbSize = fdt_totalsize (*Dtb);
+
+  return EFI_SUCCESS;
+}
