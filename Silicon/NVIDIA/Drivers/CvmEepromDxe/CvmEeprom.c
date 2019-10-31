3e5a48faae5de7ebb0028ffd5dfac27e33685e8d
Jeff Brasen <jbrasen@nvidia.com>
2019-10-31 11:48:43 -0600

    Silicon/NVIDIA: Enable driver for eeprom access
    Add driver for accessing system CVM EEPROM over I2C.
    
    Change-Id: I9e271005130716555964318988d771555220fae8
    Reviewed-on: https://git-master.nvidia.com/r/2231703
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>

---
+++ Silicon/NVIDIA/Drivers/CvmEepromDxe/CvmEeprom.c
@@ -0,0 +1,404 @@
+/** @file
+
+  CVM EEPROM Driver
+
+  Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include <PiDxe.h>
+
+#include <Library/UefiLib.h>
+#include <Library/DebugLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/BaseMemoryLib.h>
+
+#include <Protocol/DriverBinding.h>
+#include <Protocol/I2cIo.h>
+#include <Protocol/CvmEeprom.h>
+
+
+
+/**
+  Tests to see if this driver supports a given controller. If a child device is provided,
+  it further tests to see if this driver supports creating a handle for the specified child device.
+
+  This function checks to see if the driver specified by This supports the device specified by
+  ControllerHandle. Drivers will typically use the device path attached to
+  ControllerHandle and/or the services from the bus I/O abstraction attached to
+  ControllerHandle to determine if the driver supports ControllerHandle. This function
+  may be called many times during platform initialization. In order to reduce boot times, the tests
+  performed by this function must be very small, and take as little time as possible to execute. This
+  function must not change the state of any hardware devices, and this function must be aware that the
+  device specified by ControllerHandle may already be managed by the same driver or a
+  different driver. This function must match its calls to AllocatePages() with FreePages(),
+  AllocatePool() with FreePool(), and OpenProtocol() with CloseProtocol().
+  Since ControllerHandle may have been previously started by the same driver, if a protocol is
+  already in the opened state, then it must not be closed with CloseProtocol(). This is required
+  to guarantee the state of ControllerHandle is not modified by this function.
+
+  @param[in]  This                 A pointer to the EFI_DRIVER_BINDING_PROTOCOL instance.
+  @param[in]  ControllerHandle     The handle of the controller to test. This handle
+                                   must support a protocol interface that supplies
+                                   an I/O abstraction to the driver.
+  @param[in]  RemainingDevicePath  A pointer to the remaining portion of a device path.  This
+                                   parameter is ignored by device drivers, and is optional for bus
+                                   drivers. For bus drivers, if this parameter is not NULL, then
+                                   the bus driver must determine if the bus controller specified
+                                   by ControllerHandle and the child controller specified
+                                   by RemainingDevicePath are both supported by this
+                                   bus driver.
+
+  @retval EFI_SUCCESS              The device specified by ControllerHandle and
+                                   RemainingDevicePath is supported by the driver specified by This.
+  @retval EFI_ALREADY_STARTED      The device specified by ControllerHandle and
+                                   RemainingDevicePath is already being managed by the driver
+                                   specified by This.
+  @retval EFI_ACCESS_DENIED        The device specified by ControllerHandle and
+                                   RemainingDevicePath is already being managed by a different
+                                   driver or an application that requires exclusive access.
+                                   Currently not implemented.
+  @retval EFI_UNSUPPORTED          The device specified by ControllerHandle and
+                                   RemainingDevicePath is not supported by the driver specified by This.
+**/
+EFI_STATUS
+EFIAPI
+CvmEepromDxeDriverBindingSupported (
+  IN EFI_DRIVER_BINDING_PROTOCOL   *This,
+  IN EFI_HANDLE                    Controller,
+  IN EFI_DEVICE_PATH_PROTOCOL      *RemainingDevicePath
+)
+{
+  EFI_STATUS                       Status;
+  EFI_I2C_IO_PROTOCOL              *I2cIo;
+  BOOLEAN                          SupportedDevice;
+
+  // Check whether driver has already been started.
+  Status = gBS->OpenProtocol (Controller,
+                              &gEfiI2cIoProtocolGuid,
+                              (VOID**) &I2cIo,
+                              This->DriverBindingHandle,
+                              Controller,
+                              EFI_OPEN_PROTOCOL_BY_DRIVER);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  SupportedDevice = CompareGuid (&gNVIDIACvmEeprom, I2cIo->DeviceGuid);
+
+  Status = gBS->CloseProtocol (Controller,
+                               &gEfiI2cIoProtocolGuid,
+                               This->DriverBindingHandle,
+                               Controller);
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  if (SupportedDevice) {
+    Status = EFI_SUCCESS;
+  } else {
+    Status = EFI_UNSUPPORTED;
+  }
+  return Status;
+}
+
+STATIC CONST UINT8 CRC8Table[256] = {
+  0, 94, 188, 226, 97, 63, 221, 131, 194, 156, 126, 32, 163, 253, 31, 65,
+  157, 195, 33, 127, 252, 162, 64, 30, 95, 1, 227, 189, 62, 96, 130, 220,
+  35, 125, 159, 193, 66, 28, 254, 160, 225, 191, 93, 3, 128, 222, 60, 98,
+  190, 224, 2, 92, 223, 129, 99, 61, 124, 34, 192, 158, 29, 67, 161, 255,
+  70, 24, 250, 164, 39, 121, 155, 197, 132, 218, 56, 102, 229, 187, 89, 7,
+  219, 133, 103, 57, 186, 228, 6, 88, 25, 71, 165, 251, 120, 38, 196, 154,
+  101, 59, 217, 135, 4, 90, 184, 230, 167, 249, 27, 69, 198, 152, 122, 36,
+  248, 166, 68, 26, 153, 199, 37, 123, 58, 100, 134, 216, 91, 5, 231, 185,
+  140, 210, 48, 110, 237, 179, 81, 15, 78, 16, 242, 172, 47, 113, 147, 205,
+  17, 79, 173, 243, 112, 46, 204, 146, 211, 141, 111, 49, 178, 236, 14, 80,
+  175, 241, 19, 77, 206, 144, 114, 44, 109, 51, 209, 143, 12, 82, 176, 238,
+  50, 108, 142, 208, 83, 13, 239, 177, 240, 174, 76, 18, 145, 207, 45, 115,
+  202, 148, 118, 40, 171, 245, 23, 73, 8, 86, 180, 234, 105, 55, 213, 139,
+  87, 9, 235, 181, 54, 104, 138, 212, 149, 203, 41, 119, 244, 170, 72, 22,
+  233, 183, 85, 11, 136, 214, 52, 106, 43, 117, 151, 201, 74, 20, 246, 168,
+  116, 42, 200, 150, 21, 75, 169, 247, 182, 232, 10, 84, 215, 137, 107, 53};
+
+/**
+  Calculates CRC-8 for input buffer.
+
+  @param[in]  Buffer               A pointer to the data buffer.
+  @param[in]  Size                 Size of buffer.
+
+  @return the CRC-8 value.
+**/
+STATIC
+UINT8
+EFIAPI
+CalculateCrc8 (
+  IN UINT8  *Buffer,
+  IN UINT16 Size
+)
+{
+  UINT8   Crc8 = 0;
+  UINT16  Index;
+
+  for (Index = 0; Index < Size; Index++) {
+    Crc8 =  CRC8Table[Buffer[Index] ^ Crc8];
+  }
+  return Crc8;
+}
+
+/**
+  Starts a device controller or a bus controller.
+
+  The Start() function is designed to be invoked from the EFI boot service ConnectController().
+  As a result, much of the error checking on the parameters to Start() has been moved into this
+  common boot service. It is legal to call Start() from other locations,
+  but the following calling restrictions must be followed or the system behavior will not be deterministic.
+  1. ControllerHandle must be a valid EFI_HANDLE.
+  2. If RemainingDevicePath is not NULL, then it must be a pointer to a naturally aligned
+     EFI_DEVICE_PATH_PROTOCOL.
+  3. Prior to calling Start(), the Supported() function for the driver specified by This must
+     have been called with the same calling parameters, and Supported() must have returned EFI_SUCCESS.
+
+  @param[in]  This                 A pointer to the EFI_DRIVER_BINDING_PROTOCOL instance.
+  @param[in]  ControllerHandle     The handle of the controller to start. This handle
+                                   must support a protocol interface that supplies
+                                   an I/O abstraction to the driver.
+  @param[in]  RemainingDevicePath  A pointer to the remaining portion of a device path.  This
+                                   parameter is ignored by device drivers, and is optional for bus
+                                   drivers. For a bus driver, if this parameter is NULL, then handles
+                                   for all the children of Controller are created by this driver.
+                                   If this parameter is not NULL and the first Device Path Node is
+                                   not the End of Device Path Node, then only the handle for the
+                                   child device specified by the first Device Path Node of
+                                   RemainingDevicePath is created by this driver.
+                                   If the first Device Path Node of RemainingDevicePath is
+                                   the End of Device Path Node, no child handle is created by this
+                                   driver.
+
+  @retval EFI_SUCCESS              The device was started.
+  @retval EFI_DEVICE_ERROR         The device could not be started due to a device error.Currently not implemented.
+  @retval EFI_OUT_OF_RESOURCES     The request could not be completed due to a lack of resources.
+  @retval Others                   The driver failded to start the device.
+**/
+EFI_STATUS
+EFIAPI
+CvmEepromDxeDriverBindingStart (
+  IN EFI_DRIVER_BINDING_PROTOCOL   *This,
+  IN EFI_HANDLE                    Controller,
+  IN EFI_DEVICE_PATH_PROTOCOL      *RemainingDevicePath
+)
+{
+  EFI_STATUS                       Status;
+  EFI_I2C_IO_PROTOCOL              *I2cIo = NULL;
+  TEGRA_CVM_EEPROM_PROTOCOL        *EepromData = NULL;
+  BOOLEAN                          ProtocolsInstalled = FALSE;
+  EFI_I2C_REQUEST_PACKET           *Request = NULL;
+  UINT8                            Address = 0;
+  UINT8                            *RawData;
+  UINT8                            Checksum;
+
+  // Open i2cio Controller Protocol
+  Status = gBS->OpenProtocol (Controller,
+                              &gEfiI2cIoProtocolGuid,
+                              (VOID**) &I2cIo,
+                              This->DriverBindingHandle,
+                              Controller,
+                              EFI_OPEN_PROTOCOL_BY_DRIVER);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Unable to open I2cIo Protocol\n", __FUNCTION__));
+    goto ErrorExit;
+  }
+
+  // Allocate EEPROM Data
+  EepromData = (TEGRA_CVM_EEPROM_PROTOCOL *)AllocateZeroPool (sizeof (TEGRA_CVM_EEPROM_PROTOCOL));
+  if (EepromData == NULL) {
+    Status = EFI_OUT_OF_RESOURCES;
+    goto ErrorExit;
+  }
+
+  RawData = (UINT8 *)EepromData;
+  Request = (EFI_I2C_REQUEST_PACKET *)AllocateZeroPool (sizeof (EFI_I2C_REQUEST_PACKET) + sizeof (EFI_I2C_OPERATION));
+  if (Request == NULL) {
+    Status = EFI_OUT_OF_RESOURCES;
+    goto ErrorExit;
+  }
+  Request->OperationCount = 2;
+  Request->Operation[0].Flags = 0;
+  Request->Operation[0].LengthInBytes = sizeof (Address);
+  Request->Operation[0].Buffer = &Address;
+  Request->Operation[1].Flags = I2C_FLAG_READ;
+  Request->Operation[1].LengthInBytes = sizeof (TEGRA_CVM_EEPROM_PROTOCOL);
+  Request->Operation[1].Buffer = (UINT8 *)RawData;
+  Status = I2cIo->QueueRequest (I2cIo, 0, NULL, Request, NULL);
+  if (EFI_ERROR (Status)) {
+   DEBUG ((DEBUG_ERROR, "Failed to read eeprom (%r)\r\n", Status));
+   goto ErrorExit;
+  }
+  FreePool (Request);
+  Request = NULL;
+
+  if ((EepromData->Version != CVM_EEPROM_VERSION) ||
+      ((EepromData->Size + 1) != sizeof (TEGRA_CVM_EEPROM_PROTOCOL))) {
+    DEBUG ((DEBUG_ERROR, "%a: Invalid size/version in eeprom\r\n", __FUNCTION__));
+    Status = EFI_DEVICE_ERROR;
+    goto ErrorExit;
+  }
+
+  Checksum = CalculateCrc8 (RawData, EepromData->Size);
+  if (Checksum != EepromData->Checksum) {
+    DEBUG ((DEBUG_ERROR, "%a: CRC mismatch, expected %02x got %02x\r\n", __FUNCTION__, Checksum, EepromData->Checksum));
+    Status = EFI_DEVICE_ERROR;
+    goto ErrorExit;
+  }
+
+  Status = gBS->InstallMultipleProtocolInterfaces (&Controller,
+                                                   &gNVIDIACvmEepromProtocolGuid,
+                                                   EepromData,
+                                                   NULL);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to install EEPROM protocols\n", __FUNCTION__));
+    goto ErrorExit;
+  }
+  ProtocolsInstalled = TRUE;
+
+ErrorExit:
+  if (EFI_ERROR (Status)) {
+    if (ProtocolsInstalled) {
+      Status = gBS->UninstallMultipleProtocolInterfaces (Controller,
+                                                         &gNVIDIACvmEepromProtocolGuid,
+                                                         EepromData,
+                                                         NULL);
+      if (EFI_ERROR (Status)) {
+        DEBUG ((DEBUG_ERROR, "%a: Failed to uninstall EEPROM protocols\n", __FUNCTION__));
+      }
+    }
+    if (EepromData != NULL) {
+      FreePool (EepromData);
+    }
+    if (Request != NULL) {
+      FreePool (Request);
+    }
+    if (I2cIo != NULL) {
+      gBS->CloseProtocol (Controller,
+                          &gEfiI2cIoProtocolGuid,
+                          This->DriverBindingHandle,
+                          I2cIo);
+    }
+  }
+
+  return Status;
+}
+
+
+/**
+  Stops a device controller or a bus controller.
+
+  The Stop() function is designed to be invoked from the EFI boot service DisconnectController().
+  As a result, much of the error checking on the parameters to Stop() has been moved
+  into this common boot service. It is legal to call Stop() from other locations,
+  but the following calling restrictions must be followed or the system behavior will not be deterministic.
+  1. ControllerHandle must be a valid EFI_HANDLE that was used on a previous call to this
+     same driver's Start() function.
+  2. The first NumberOfChildren handles of ChildHandleBuffer must all be a valid
+     EFI_HANDLE. In addition, all of these handles must have been created in this driver's
+     Start() function, and the Start() function must have called OpenProtocol() on
+     ControllerHandle with an Attribute of EFI_OPEN_PROTOCOL_BY_CHILD_CONTROLLER.
+
+  @param[in]  This              A pointer to the EFI_DRIVER_BINDING_PROTOCOL instance.
+  @param[in]  ControllerHandle  A handle to the device being stopped. The handle must
+                                support a bus specific I/O protocol for the driver
+                                to use to stop the device.
+  @param[in]  NumberOfChildren  The number of child device handles in ChildHandleBuffer.
+  @param[in]  ChildHandleBuffer An array of child handles to be freed. May be NULL
+                                if NumberOfChildren is 0.
+
+  @retval EFI_SUCCESS           The device was stopped.
+  @retval EFI_DEVICE_ERROR      The device could not be stopped due to a device error.
+**/
+EFI_STATUS
+EFIAPI
+CvmEepromDxeDriverBindingStop (
+  IN  EFI_DRIVER_BINDING_PROTOCOL     *This,
+  IN  EFI_HANDLE                      Controller,
+  IN  UINTN                           NumberOfChildren,
+  IN  EFI_HANDLE                      *ChildHandleBuffer
+)
+{
+  EFI_STATUS                Status;
+  EFI_I2C_IO_PROTOCOL       *I2cIo = NULL;
+  TEGRA_CVM_EEPROM_PROTOCOL *EepromData = NULL;
+
+  Status = gBS->HandleProtocol (Controller, &gEfiI2cIoProtocolGuid, (VOID **)&I2cIo);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get i2cio protocol (%r)\r\n", __FUNCTION__, Status));
+    return Status;
+  }
+
+  Status = gBS->HandleProtocol (Controller, &gNVIDIACvmEepromProtocolGuid, (VOID **)&EepromData);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get eeprom protocol (%r)\r\n", __FUNCTION__, Status));
+    return Status;
+  }
+
+  Status = gBS->UninstallMultipleProtocolInterfaces (Controller,
+                                                     &gNVIDIACvmEepromProtocolGuid,
+                                                     EepromData,
+                                                     NULL);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to uninstall eeprom protocol (%r)\r\n", __FUNCTION__, Status));
+    return Status;
+  }
+  FreePool (EepromData);
+
+  Status = gBS->CloseProtocol (Controller,
+                               &gEfiI2cIoProtocolGuid,
+                               This->DriverBindingHandle,
+                               I2cIo);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to close i2cio protocol (%r)\r\n", __FUNCTION__, Status));
+    return Status;
+  }
+  return EFI_SUCCESS;
+}
+
+
+EFI_DRIVER_BINDING_PROTOCOL gCvmEepromDxeDriverBinding = {
+  CvmEepromDxeDriverBindingSupported,
+  CvmEepromDxeDriverBindingStart,
+  CvmEepromDxeDriverBindingStop,
+  0x1,
+  NULL,
+  NULL
+};
+
+
+/**
+  The user Entry Point for module NorFlashDxe. The user code starts with this function.
+
+  @param[in] ImageHandle    The firmware allocated handle for the EFI image.
+  @param[in] SystemTable    A pointer to the EFI System Table.
+
+  @retval EFI_SUCCESS       The entry point is executed successfully.
+  @retval other             Some errors occur when executing this entry point.
+**/
+EFI_STATUS
+EFIAPI
+InitializeCvmEepromDxe (
+  IN EFI_HANDLE           ImageHandle,
+  IN EFI_SYSTEM_TABLE     *SystemTable
+)
+{
+  // TODO: Add component name support.
+  return EfiLibInstallDriverBinding (ImageHandle,
+                                     SystemTable,
+                                     &gCvmEepromDxeDriverBinding,
+                                     ImageHandle);
+}
---
+++ Silicon/NVIDIA/Include/Protocol/CvmEeprom.h
@@ -0,0 +1,129 @@
+/** @file
+  NVIDIA CVM EEPROM Protocol
+
+  Copyright (c) 2019, NVIDIA CORPORATION. All rights reserved.
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#ifndef __NVIDIA_CVM_EEPROM_PROTOCOL_H__
+#define __NVIDIA_CVM_EEPROM_PROTOCOL_H__
+
+
+#define CVM_EEPROM_VERSION                  1
+#define CVM_EEPROM_CUSTOMER_BLOCK_SIGNATURE "NVCB"
+#define CVM_EEPROM_CUSTOMER_TYPE_SIGNATURE  "M1"
+/**
+ * @brief The Product Part Number structure that is embedded into
+ * EEPROM layout structure
+ *
+ * @param Leading - 699 or 600 sticker info
+ * @param Separator0 - separator, always '-'
+ * @param Class - Board Class, always 8 for mobile
+ * @param Id - Board ID (Quill Product 3310)
+ * @param Separator1 - separator, always '-'
+ * @param Sku - Board SKU
+ * @param Separator2 - separator, always '-'
+ * @param Fab - FAB value, Eg: 100, 200, 300
+ * @param Separator3 - separator, space ' '
+ * @param Revision - Manufacturing Major revision
+ * @param Separator4 - separator, always '.'
+ * @param Ending - Always "0"
+ * @param Pad - 0x00
+ */
+#pragma pack(1)
+typedef struct {       /* 20 - 49 */
+  UINT8  Leading[3];   /* 20 */
+  UINT8  Separator0;   /* 23 */
+  UINT8  Class;        /* 24 */
+  UINT8  Id[4];        /* 25 */
+  UINT8  Separator1;   /* 29 */
+  UINT8  Sku[4];       /* 30 */
+  UINT8  Separator2;   /* 34 */
+  UINT8  Fab[3];       /* 35 */
+  UINT8  Separator3;   /* 38 */
+  UINT8  Revision;     /* 39 */
+  UINT8  Separator4;   /* 40 */
+  UINT8  Ending;       /* 41 */
+  UINT8  Pad[8];       /* 42 */
+} TEGRA_EEPROM_PART_NUMBER;
+#pragma pack()
+
+/**
+ * @brief The layout of data in EEPROMS
+ *
+ * @param Version - Version of Board ID contents
+ * @param Size - Size of Board ID data that follows this address
+ * @param BoardNumber - ID of the board on which EEPROM in mounted
+ * @param Sku - Always matches Board SKU on sticker
+ * @param Fab - fabrication ID of the Board
+ * @param Revision - revision of the Board
+ * @param MinorRevision - Minor revision
+ * @param MemoryType - Memory type
+ * @param PowerConfig - Power cfgs like PM Stuff, DC-DC, VF, Max * Curr Limits
+ * @param MiscConfig - Defines spl reworks, mech. Changes. Its a bitwise field
+ * @param ModemConfig - Modem, eg: Icera Modem fuse/unfuse, Antenna bands
+ * @param TouchConfig - Reworks related to touch
+ * @param DisplayConfig - Reflects any spl reworks/changes related to Display
+ * @param ReworkLevel - Syseng Rework Level
+ * @param Reserved0 - Reserved bytes
+ * @param PartNumber - asset_tracker_field_1 - 699 or 600 BOM Number
+ * @param WifiMacAddress - MAC address for primary wifi chip
+ * @param BtMacAddress - MAC address for bluetooth chip
+ * @param SecWifiMacAddress - MAC address for secondary wifi chip
+ * @param EthernetMacAddress - MAC address for ethernet port
+ * @param SerialNumber - asset_field_tracker_2 - Serial number on sticker
+ * @param Reserved1 - Reserved bytes
+ * @param CustomerBlockSignature - 'NVCB' - NV Config Block
+ * @param CustomerBlockLength - Length from Block Signature to end of EEPROM
+ * @param CustomerTypeSignature - 'M1' - MAC Address Struc Type 1
+ * @param CustomerVersion - 0x0000
+ * @param CustomerWifiMacAddress - Customer usable field
+ * @param CustomerBtMacAddress - Customer usable field
+ * @param CustomerEthernetMacAddress - Customer usable field
+ * @param Reserved2 - Reserved for future use
+ * @param Checksum - CRC-8 computed for bytes 0 through 254
+ */
+#pragma pack(1)
+typedef struct {
+  UINT16   Version;                       /* 00 */
+  UINT16   Size;                          /* 02 */
+  UINT16   BoardNumber;                   /* 04 */
+  UINT16   Sku;                           /* 06 */
+  UINT8    Fab;                           /* 08 */
+  UINT8    Revision;                      /* 09 */
+  UINT8    MinorRevision;                 /* 10 */
+  UINT8    MemoryType;                    /* 11 */
+  UINT8    PowerConfig;                   /* 12 */
+  UINT8    MiscConfig;                    /* 13 */
+  UINT8    ModemConfig;                   /* 14 */
+  UINT8    TouchConfig;                   /* 15 */
+  UINT8    DisplayConfig;                 /* 16 */
+  UINT8    ReworkLevel;                   /* 17 */
+  UINT8    Reserved0[2];                  /* 18 */
+  TEGRA_EEPROM_PART_NUMBER PartNumber;    /* 20 - 49 */
+  UINT8    WifiMacAddress[6];             /* 50 */
+  UINT8    BtMacAddress[6];               /* 56 */
+  UINT8    SecWifiMacAddress[6];          /* 62 */
+  UINT8    EthernetMacAddress[6];         /* 68 */
+  UINT8    SerialNumber[15];              /* 74 */
+  UINT8    Reserved1[61];                 /* 89 */
+  UINT8    CustomerBlockSignature[4];     /* 150 */
+  UINT16   CustomerBlockLength;           /* 154 */
+  UINT8    CustomerTypeSignature[2];      /* 156 */
+  UINT16   CustomerVersion;               /* 158 */
+  UINT8    CustomerWifiMacAddress[6];     /* 160 */
+  UINT8    CustomerBtMacAddress[6];       /* 166 */
+  UINT8    CustomerEthernetMacAddress[6]; /* 172 */
+  UINT8    Reserved2[77];                 /* 178 */
+  UINT8    Checksum;                      /* 255 */
+} TEGRA_CVM_EEPROM_PROTOCOL;
+#pragma pack()
+
+#endif
