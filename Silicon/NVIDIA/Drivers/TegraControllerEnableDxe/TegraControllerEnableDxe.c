25bb46d5de1402127e1e9b96aca9186df012492f
Ashish Singhal <ashishsingha@nvidia.com>
2020-03-09 11:01:31 -0600

    NVIDIA/TegraCoreEnableDxe: Add Tegra Controller Enable Driver
    This is a new driver that takes care of clocking and releasing a
    HW controller from reset so that it can be used by later SW stages
    without worrying about enabling the core.
    
    Change-Id: I37ff7219f2b329fc15a86c29f6408b17e665c6be
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/edk2-platforms/+/2305828
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: Laxman Dewangan <ldewangan@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

---
+++ Silicon/NVIDIA/Drivers/TegraControllerEnableDxe/TegraControllerEnableDxe.c
@@ -0,0 +1,58 @@
+/** @file
+
+  Tegra Controller Enable Driver
+
+  Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include <Library/DeviceDiscoveryDriverLib.h>
+
+NVIDIA_COMPATIBILITY_MAPPING gDeviceCompatibilityMap[] = {
+    { "nvidia,tegra186-spi", &gNVIDIANonDiscoverableT186SpiDeviceGuid },
+    { "nvidia,tegra194-spi", &gNVIDIANonDiscoverableT194SpiDeviceGuid },
+    { NULL, NULL }
+};
+
+NVIDIA_DEVICE_DISCOVERY_CONFIG gDeviceDiscoverDriverConfig = {
+    .DriverName = L"NVIDIA Controller Enable Driver",
+    .UseDriverBinding = TRUE,
+    .AutoEnableClocks = TRUE,
+    .AutoResetModule = TRUE,
+    .SkipEdkiiNondiscoverableInstall = FALSE
+};
+
+
+/**
+  Callback that will be invoked at various phases of the driver initialization
+
+  This function allows for modification of system behavior at various points in
+  the driver binding process.
+
+  @param[in] Phase                    Current phase of the driver initialization
+  @param[in] DriverHandle             Handle of the driver.
+  @param[in] ControllerHandle         Handle of the controller.
+  @param[in] DeviceTreeNode           Pointer to the device tree node protocol is available.
+
+  @retval EFI_SUCCESS              Operation successful.
+  @retval EFI_SUCCESS              Driver does not handle this phase
+  @retval others                   Error occurred
+
+**/
+EFI_STATUS
+DeviceDiscoveryNotify (
+  IN  NVIDIA_DEVICE_DISCOVERY_PHASES         Phase,
+  IN  EFI_HANDLE                             DriverHandle,
+  IN  EFI_HANDLE                             ControllerHandle,
+  IN  CONST NVIDIA_DEVICE_TREE_NODE_PROTOCOL *DeviceTreeNode OPTIONAL
+  )
+{
+  return EFI_SUCCESS;
+}
