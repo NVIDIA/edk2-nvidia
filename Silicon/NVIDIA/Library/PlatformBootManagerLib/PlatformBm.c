efec11362100e43ec8deee75c030fd01def0c52c
Jeff Brasen <jbrasen@nvidia.com>
2020-07-20 10:08:35 -0600

    Silicon/NVIDIA/PlatformBootManager: Add Memory Test
    Invoke the memory test if supported on system.
    
    Change-Id: I7ba74088fe8524d2dd8ff2d99001c43a3bf0e6c9
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/edk2-platforms/+/2382462
    Tested-by: Jeff Brasen <jbrasen@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- Silicon/NVIDIA/Library/PlatformBootManagerLib/PlatformBm.c
+++ Silicon/NVIDIA/Library/PlatformBootManagerLib/PlatformBm.c
@@ -26,6 +26,7 @@
 #include <Library/PrintLib.h>
 #include <Protocol/DevicePath.h>
 #include <Protocol/EsrtManagement.h>
+#include <Protocol/GenericMemoryTest.h>
 #include <Protocol/GraphicsOutput.h>
 #include <Protocol/LoadedImage.h>
 #include <Protocol/PciIo.h>
@@ -174,6 +175,78 @@ FilterAndProcess (
   gBS->FreePool (Handles);
 }
 
+/**
+  Perform the memory test base on the memory test intensive level,
+  and update the memory resource.
+
+  @param  Level         The memory test intensive level.
+
+  @retval EFI_STATUS    Success test all the system memory and update
+                        the memory resource
+
+**/
+EFI_STATUS
+MemoryTest (
+  IN EXTENDMEM_COVERAGE_LEVEL Level
+  )
+{
+  EFI_STATUS                        Status;
+  BOOLEAN                           RequireSoftECCInit;
+  EFI_GENERIC_MEMORY_TEST_PROTOCOL  *GenMemoryTest;
+  UINT64                            TestedMemorySize;
+  UINT64                            TotalMemorySize;
+  BOOLEAN                           ErrorOut;
+  BOOLEAN                           TestAbort;
+
+  TestedMemorySize  = 0;
+  TotalMemorySize   = 0;
+  ErrorOut          = FALSE;
+  TestAbort         = FALSE;
+
+  RequireSoftECCInit = FALSE;
+
+  Status = gBS->LocateProtocol (
+                  &gEfiGenericMemTestProtocolGuid,
+                  NULL,
+                  (VOID **) &GenMemoryTest
+                  );
+  if (EFI_ERROR (Status)) {
+    return EFI_SUCCESS;
+  }
+
+  Status = GenMemoryTest->MemoryTestInit (
+                                GenMemoryTest,
+                                Level,
+                                &RequireSoftECCInit
+                                );
+  if (Status == EFI_NO_MEDIA) {
+    //
+    // The PEI codes also have the relevant memory test code to check the memory,
+    // it can select to test some range of the memory or all of them. If PEI code
+    // checks all the memory, this BDS memory test will has no not-test memory to
+    // do the test, and then the status of EFI_NO_MEDIA will be returned by
+    // "MemoryTestInit". So it does not need to test memory again, just return.
+    //
+    return EFI_SUCCESS;
+  }
+
+  do {
+    Status = GenMemoryTest->PerformMemoryTest (
+                              GenMemoryTest,
+                              &TestedMemorySize,
+                              &TotalMemorySize,
+                              &ErrorOut,
+                              TestAbort
+                              );
+    if (ErrorOut && (Status == EFI_DEVICE_ERROR)) {
+      ASSERT (0);
+    }
+  } while (Status != EFI_NOT_FOUND);
+
+  Status = GenMemoryTest->Finished (GenMemoryTest);
+
+  return EFI_SUCCESS;
+}
 
 /**
   This FILTER_FUNCTION checks if a handle corresponds to a PCI display device.
@@ -699,7 +772,7 @@ PlatformBootManagerBeforeConsole (
     //
     PlatformRegisterConsoles ();
 
-	//
+    //
     // Set platform has been configured
     //
     PlatformConfigured ();
@@ -813,6 +886,9 @@ PlatformBootManagerAfterConsole (
         PcdGetPtr (PcdFirmwareVersionString));
     }
   }
+
+  //Run Sparse memory test
+  MemoryTest (SPARSE);
 
   //
   // On ARM, there is currently no reason to use the phased capsule
