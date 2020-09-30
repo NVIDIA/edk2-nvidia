1062ef71c55629fda02796fa0fab333c10a5bb66
Ashish Singhal <ashishsingha@nvidia.com>
2020-09-29 21:36:18 -0600

    NVIDIA: Add NS SMM Interface Support
    Add non-secure side support for being able to communicate with
    SMM if it is available. If not, use pre-existing drivers.
    
    Bug 3144166
    
    Change-Id: If6cb5313f13149b03c895b151db345f0d0702c54
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/edk2-platforms/+/2421603
    Reviewed-by: automaticguardword <automaticguardword@nvidia.com>
    Reviewed-by: Mayur Gudmeti <mgudmeti@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Mayur Gudmeti <mgudmeti@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

---
+++ Silicon/NVIDIA/Library/StandaloneMmUnavailableLib/StandaloneMmUnavailableLib.c
@@ -0,0 +1,41 @@
+/** @file
+This module installs smm unavailable protocol.
+
+Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+
+SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Library/DebugLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+
+/**
+  The destructor function installs smm unavailable protocol
+
+  @param  ImageHandle   The firmware allocated handle for the EFI image.
+  @param  SystemTable   A pointer to the Management mode System Table.
+
+  @retval EFI_SUCCESS   The constructor always returns EFI_SUCCESS.
+
+**/
+EFI_STATUS
+EFIAPI
+StandaloneMmUnavailableLibDestructor (
+  IN EFI_HANDLE                           ImageHandle,
+  IN EFI_SYSTEM_TABLE                     *SystemTable
+  )
+{
+  EFI_STATUS            Status;
+  EFI_HANDLE            Handle;
+
+  Handle = NULL;
+  Status = gBS->InstallMultipleProtocolInterfaces (
+                  &Handle,
+                  &gNVIDIAStandaloneMmUnavailableGuid,
+                  NULL,
+                  NULL
+                  );
+  ASSERT_EFI_ERROR (Status);
+  return EFI_SUCCESS;
+}
