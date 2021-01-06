c296564107a574879959805b19d9b02d0ff1c8b6
Vidya Sagar <vidyas@nvidia.com>
2021-01-06 14:46:39 +0530

    Silicon/NVIDIA/PCIe: Add support for T234
    Adds the basic support required to enable PCIe in T234
    
    Bug 3139050
    
    Change-Id: I6b47afb96d63fde5847b7487cc24a6ed8d4e4c3f
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/edk2-platforms/+/2466694
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Vidya Sagar <vidyas@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- Silicon/NVIDIA/Drivers/PcieControllerDxe/PcieControllerDxe.c
+++ Silicon/NVIDIA/Drivers/PcieControllerDxe/PcieControllerDxe.c
@@ -2,7 +2,7 @@
 
   PCIe Controller Driver
 
-  Copyright (c) 2019-2020, NVIDIA CORPORATION. All rights reserved.
+  Copyright (c) 2019-2021, NVIDIA CORPORATION. All rights reserved.
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -46,6 +46,7 @@
 
 NVIDIA_COMPATIBILITY_MAPPING gDeviceCompatibilityMap[] = {
     { "nvidia,tegra194-pcie", &gNVIDIANonDiscoverableT194PcieDeviceGuid },
+    { "nvidia,tegra234-pcie", &gNVIDIANonDiscoverableT234PcieDeviceGuid },
     { NULL, NULL }
 };
 
