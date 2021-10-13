2fd693a500b13be8a27742fa6575a644e08e6de3
Bob Morgan <bobm@nvidia.com>
2021-10-12 21:17:48 -0600

    NVIDIA/BootChainInfoLib: Add T194 and name conversion functions
    Add T194 support and additional name conversion functions.
    
    Change-Id: I48ef7d3375bbdb3767b5d579f8c49b53a6fde001
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/edk2-platforms/+/2609835
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Bob Morgan <bobm@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: mobile promotions <svcmobile_promotions@nvidia.com>
    Tested-by: Bob Morgan <bobm@nvidia.com>
    Tested-by: mobile promotions <svcmobile_promotions@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- Silicon/NVIDIA/Include/Library/BootChainInfoLib.h
+++ Silicon/NVIDIA/Include/Library/BootChainInfoLib.h
@@ -37,6 +37,14 @@
 /**
   Retrieve Active Boot Chain Partition Name
 
+  @param[in]  GeneralPartitionName  Pointer to general partition name string
+  @param[out] ActivePartitionName   Pointer to buffer of MAX_PARTITION_NAME_LEN
+                                    CHAR16 characters to contain the
+                                    active boot chain partition name
+
+  @retval EFI_SUCCESS               Operation successful.
+  @retval others                    Error occurred.
+
 **/
 EFI_STATUS
 EFIAPI
@@ -44,5 +52,46 @@ GetActivePartitionName (
   IN  CONST CHAR16 *GeneralPartitionName,
   OUT CHAR16       *ActivePartitionName
 );
+
+/**
+  Get boot chain partition name from base partition name and boot chain index.
+
+  @param[in]  BasePartitionName         Pointer to base partition name string
+  @param[in]  BootChain                 Boot chain (0=a, 1=b)
+  @param[out] BootChainPartitionName    Pointer to buffer of MAX_PARTITION_NAME_LEN
+                                        CHAR16 characters to contain the
+                                        boot chain partition name
+
+  @retval EFI_SUCCESS                   Operation successful.
+  @retval others                        Error occurred.
+
+**/
+EFI_STATUS
+EFIAPI
+GetBootChainPartitionName (
+  IN  CONST CHAR16      *BasePartitionName,
+  IN  UINTN             BootChain,
+  OUT CHAR16            *BootChainPartitionName
+  );
+
+/**
+  Get base name and boot chain index from partition name
+
+  @param[in]  PartitionName         Pointer to partition name string
+  @param[out] BaseName              Pointer to buffer of MAX_PARTITION_NAME_LEN
+                                    CHAR16 characters to contain the
+                                    base partition name
+  @param[out] BootChain             Pointer to boot chain (0=a, 1=b)
+
+  @retval EFI_SUCCESS               Operation successful.
+  @retval others                    Error occurred.
+**/
+EFI_STATUS
+EFIAPI
+GetPartitionBaseNameAndBootChain (
+  IN  CONST CHAR16      *PartitionName,
+  OUT CHAR16            *BaseName,
+  OUT UINTN             *BootChain
+  );
 
 #endif
--- Silicon/NVIDIA/Library/BootChainInfoLib/BootChainInfoLib.c
+++ Silicon/NVIDIA/Library/BootChainInfoLib/BootChainInfoLib.c
@@ -33,11 +33,59 @@
 #include <Library/BaseLib.h>
 
 #define MAX_BOOT_CHAIN_INFO_MAPPING 2
+#define PARTITION_PREFIX_LENGTH     2
 
 CHAR16 *T234PartitionNameId[MAX_BOOT_CHAIN_INFO_MAPPING] = {
   L"A_",
   L"B_",
 };
+
+CHAR16 *T194PartitionNameId[MAX_BOOT_CHAIN_INFO_MAPPING] = {
+  L"",
+  L"_b",
+};
+
+EFI_STATUS
+EFIAPI
+GetBootChainPartitionName (
+  IN  CONST CHAR16      *BasePartitionName,
+  IN  UINTN             BootChain,
+  OUT CHAR16            *BootChainPartitionName
+  )
+{
+  UINTN      ChipID;
+  CHAR16     *Identifier;
+
+  if ((BasePartitionName == NULL) || (BootChainPartitionName == NULL) ||
+      (BootChain >= MAX_BOOT_CHAIN_INFO_MAPPING)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  ChipID = TegraGetChipID ();
+  switch (ChipID) {
+    case T234_CHIP_ID:
+      Identifier = T234PartitionNameId[BootChain];
+      UnicodeSPrint (BootChainPartitionName,
+                     sizeof (CHAR16) * MAX_PARTITION_NAME_LEN,
+                     L"%s%s",
+                     Identifier,
+                     BasePartitionName);
+      break;
+    case T194_CHIP_ID:
+      Identifier = T194PartitionNameId[BootChain];
+      UnicodeSPrint (BootChainPartitionName,
+                     sizeof (CHAR16) * MAX_PARTITION_NAME_LEN,
+                     L"%s%s",
+                     BasePartitionName,
+                     Identifier);
+      break;
+    default:
+      return EFI_UNSUPPORTED;
+      break;
+  }
+
+  return EFI_SUCCESS;
+}
 
 /**
   Retrieve Active Boot Chain Partition Name
@@ -51,27 +99,75 @@ GetActivePartitionName (
 )
 {
   EFI_STATUS Status;
-  UINTN      ChipID;
   UINT32     BootChain;
-  CHAR16     *Identifier;
+
+  if ((GeneralPartitionName == NULL) || (ActivePartitionName == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
 
   Status = GetActiveBootChain (&BootChain);
   if (EFI_ERROR (Status)) {
-    UnicodeSPrint (ActivePartitionName, sizeof (CHAR16) * MAX_PARTITION_NAME_LEN, L"%s", GeneralPartitionName);
     return Status;
   }
 
-  ChipID = TegraGetChipID ();
+  return GetBootChainPartitionName (GeneralPartitionName,
+                                    BootChain,
+                                    ActivePartitionName);
+}
 
+EFI_STATUS
+EFIAPI
+GetPartitionBaseNameAndBootChain (
+  IN  CONST CHAR16      *PartitionName,
+  OUT CHAR16            *BaseName,
+  OUT UINTN             *BootChain
+  )
+{
+  UINTN         ChipID;
+  UINTN         Index;
+
+  if ((PartitionName == NULL) || (BaseName == NULL) || (BootChain == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  ChipID = TegraGetChipID ();
   switch (ChipID) {
-    case T234_CHIP_ID:
-      Identifier = T234PartitionNameId[BootChain];
-      UnicodeSPrint (ActivePartitionName, sizeof (CHAR16) * MAX_PARTITION_NAME_LEN, L"%s%s", Identifier, GeneralPartitionName);
-      break;
+    case T234_CHIP_ID: {
+      for (Index = 0; Index < MAX_BOOT_CHAIN_INFO_MAPPING; Index++) {
+        if (StrnCmp (PartitionName, T234PartitionNameId[Index], PARTITION_PREFIX_LENGTH) == 0) {
+          StrCpyS (BaseName, MAX_PARTITION_NAME_LEN, PartitionName + PARTITION_PREFIX_LENGTH);
+          *BootChain = Index;
+          return EFI_SUCCESS;
+        }
+      }
+    }
+    break;
+    case T194_CHIP_ID: {
+      CONST CHAR16  *BSuffix;
+      UINTN         BSuffixLength;
+      CONST CHAR16  *SuffixStart;
+      UINTN         NameLength;
+
+      // check if boot chain B suffix is present, if not, it's boot chain A
+      BSuffix = T194PartitionNameId[1];
+      BSuffixLength = StrLen (BSuffix);
+      NameLength = StrLen (PartitionName);
+      if (NameLength > BSuffixLength) {
+        SuffixStart = PartitionName + NameLength - BSuffixLength;
+        if (StrnCmp (SuffixStart, BSuffix, BSuffixLength) == 0) {
+          StrnCpyS (BaseName, MAX_PARTITION_NAME_LEN, PartitionName,
+                    NameLength - BSuffixLength);
+          *BootChain = 1;
+          return EFI_SUCCESS;
+        }
+      }
+      StrCpyS (BaseName, MAX_PARTITION_NAME_LEN, PartitionName);
+      *BootChain = 0;
+      return EFI_SUCCESS;
+    }
     default:
-      UnicodeSPrint (ActivePartitionName, sizeof (CHAR16) * MAX_PARTITION_NAME_LEN, L"%s", GeneralPartitionName);
       return EFI_UNSUPPORTED;
   }
 
-  return EFI_SUCCESS;
+  return EFI_NOT_FOUND;
 }
