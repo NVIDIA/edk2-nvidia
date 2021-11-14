cca2b0703c06a09a2c154c271dd080c9ba1692f9
Ashish Singhal <ashishsingha@nvidia.com>
2021-11-13 21:59:19 -0700

    NVIDIA: Allow Boot Order Hierarchy
    Allow boot order hierarchy for multiple boot media.
    
    Bug 3510952
    Bug 3444397
    
    Change-Id: I2b43026f8dd40007bc1fafeb15e074baaf800a03
    Reviewed-on: https://git-master.nvidia.com/r/c/3rdparty/edk2-platforms/+/2625616
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: svcacv <svcacv@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: svc-sw-mobile-l4t <svc-sw-mobile-l4t@nvidia.com>
    GVS: Gerrit_Virtual_Submit

--- Silicon/NVIDIA/Library/PlatformBootOrderLib/PlatformBootOrderLib.c
+++ Silicon/NVIDIA/Library/PlatformBootOrderLib/PlatformBootOrderLib.c
@@ -1,12 +1,12 @@
 /** @file
 *
-*  Copyright (c) 2020, NVIDIA CORPORATION. All rights reserved.
+*  Copyright (c) 2020-2022, NVIDIA CORPORATION. All rights reserved.
 *  Copyright (c) 2017, Linaro Limited. All rights reserved.
 *
 *  SPDX-License-Identifier: BSD-2-Clause-Patent
 *
 *  Portions provided under the following terms:
-*  Copyright (c) 2020 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+*  Copyright (c) 2020-2022 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 *
 *  NVIDIA CORPORATION, its affiliates and licensors retain all intellectual
 *  property and proprietary rights in and to this material, related
@@ -15,7 +15,7 @@
 *  without an express license agreement from NVIDIA CORPORATION or
 *  its affiliates is strictly prohibited.
 *
-*  SPDX-FileCopyrightText: Copyright (c) 2020 NVIDIA CORPORATION & AFFILIATES
+*  SPDX-FileCopyrightText: Copyright (c) 2020-2022 NVIDIA CORPORATION & AFFILIATES
 *  SPDX-License-Identifier: LicenseRef-NvidiaProprietary
 *
 **/
@@ -27,38 +27,121 @@
 #include <Library/UefiBootManagerLib.h>
 #include <Library/UefiLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
+#include <Library/DevicePathLib.h>
+
+
+UINT8 RemovableReversePriorityBootOrder[] = {
+  MSG_SD_DP,
+  MSG_USB_DP
+};
+
+
+UINT8 NonRemovableReversePriorityBootOrder[] = {
+  MSG_UFS_DP,
+  MSG_EMMC_DP
+};
 
 
 VOID
 EFIAPI
-SetBootOrder (
+SetMediaBootPriority (
+  IN UINT8 DeviceSubType
+  )
+{
+  EFI_STATUS                   Status;
+  UINT16                       *BootOrder;
+  UINTN                        BootOrderSize;
+  UINT16                       *NewOrder;
+  UINT16                       *RemainBoots;
+  UINTN                        SelectCnt;
+  UINTN                        RemainCnt;
+  UINTN                        Index;
+  BOOLEAN                      NewOrderFound;
+  CHAR16                       OptionName[sizeof ("Boot####")];
+  EFI_BOOT_MANAGER_LOAD_OPTION Option;
+  EFI_DEVICE_PATH_PROTOCOL     *DevicePathNode;
+
+  GetEfiGlobalVariable2 (L"BootOrder", (VOID **) &BootOrder, &BootOrderSize);
+  if (BootOrder == NULL) {
+    return;
+  }
+
+  NewOrder = AllocatePool (BootOrderSize);
+  RemainBoots = AllocatePool (BootOrderSize);
+  if ((NewOrder == NULL) || (RemainBoots == NULL)) {
+    goto Exit;
+  }
+
+  SelectCnt = 0;
+  RemainCnt = 0;
+
+  for (Index = 0; Index < BootOrderSize / sizeof (UINT16); Index++) {
+    NewOrderFound = FALSE;
+    UnicodeSPrint (OptionName, sizeof (OptionName), L"Boot%04x", BootOrder[Index]);
+    Status = EfiBootManagerVariableToLoadOption (OptionName, &Option);
+    if (EFI_ERROR (Status)) {
+      continue;
+    }
+
+    DevicePathNode = Option.FilePath;
+    while (!IsDevicePathEndType (DevicePathNode)) {
+      if ((DevicePathType (DevicePathNode) == MESSAGING_DEVICE_PATH) &&
+          (DevicePathSubType (DevicePathNode) == DeviceSubType)) {
+        NewOrder[SelectCnt++] = BootOrder[Index];
+        NewOrderFound = TRUE;
+        break;
+      }
+      DevicePathNode = NextDevicePathNode (DevicePathNode);
+    }
+
+    if (!NewOrderFound) {
+      RemainBoots[RemainCnt++] = BootOrder[Index];
+    }
+  }
+
+  if (SelectCnt != 0) {
+    for (Index = 0; Index < RemainCnt; Index++) {
+      NewOrder[SelectCnt + Index] = RemainBoots[Index];
+    }
+
+    if (CompareMem (NewOrder, BootOrder, BootOrderSize) != 0) {
+      Status = gRT->SetVariable (L"BootOrder", &gEfiGlobalVariableGuid,
+                      EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS | EFI_VARIABLE_NON_VOLATILE,
+                      BootOrderSize, NewOrder);
+      if (EFI_ERROR (Status)) {
+        goto Exit;
+      }
+    }
+  }
+
+Exit:
+  FreePool (BootOrder);
+  if (NewOrder != NULL) {
+    FreePool (NewOrder);
+  }
+  if (RemainBoots != NULL) {
+    FreePool (RemainBoots);
+  }
+}
+
+
+VOID
+EFIAPI
+SetAndroidBootPriority (
   VOID
   )
 {
   EFI_STATUS                   Status;
-  BOOLEAN                      VariableData;
-  UINTN                        VariableSize;
-  UINT32                       VariableAttributes;
-  UINT16                       *NewOrder;
-  UINT16                       *RemainBoots;
   UINT16                       *BootOrder;
   UINTN                        BootOrderSize;
+  UINT16                       *NewOrder;
+  UINT16                       *RemainBoots;
+  UINTN                        SelectCnt;
+  UINTN                        RemainCnt;
   EFI_BOOT_MANAGER_LOAD_OPTION Option;
   CHAR16                       OptionName[sizeof ("Boot####")];
   UINTN                        Index;
-  UINTN                        SelectCnt;
-  UINTN                        RemainCnt;
   UINTN                        OptionalDataSize;
-
-  VariableData = FALSE;
-  VariableSize = sizeof (BOOLEAN);
-  Status = gRT->GetVariable (L"PlatformBootOrderSet", &gNVIDIATokenSpaceGuid,
-                             &VariableAttributes, &VariableSize, (VOID *)&VariableData);
-  if (!EFI_ERROR (Status) && (VariableSize == sizeof (BOOLEAN))) {
-    if (VariableData == TRUE) {
-      return;
-    }
-  }
 
   GetEfiGlobalVariable2 (L"BootOrder", (VOID **) &BootOrder, &BootOrderSize);
   if (BootOrder == NULL) {
@@ -107,13 +190,6 @@ SetBootOrder (
       if (EFI_ERROR (Status)) {
         goto Exit;
       }
-      VariableData = TRUE;
-      Status = gRT->SetVariable (L"PlatformBootOrderSet", &gNVIDIATokenSpaceGuid,
-                      EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_NON_VOLATILE,
-                      sizeof (BOOLEAN), &VariableData);
-      if (EFI_ERROR (Status)) {
-        goto Exit;
-      }
     }
   }
 
@@ -125,4 +201,51 @@ Exit:
   if (RemainBoots != NULL) {
     FreePool (RemainBoots);
   }
+}
+
+
+VOID
+EFIAPI
+SetBootOrder (
+  VOID
+  )
+{
+  EFI_STATUS                   Status;
+  BOOLEAN                      VariableData;
+  UINTN                        VariableSize;
+  UINT32                       VariableAttributes;
+  UINTN                        Count;
+
+  VariableData = FALSE;
+  VariableSize = sizeof (BOOLEAN);
+  Status = gRT->GetVariable (L"PlatformBootOrderSet", &gNVIDIATokenSpaceGuid,
+                             &VariableAttributes, &VariableSize, (VOID *)&VariableData);
+  if (!EFI_ERROR (Status) && (VariableSize == sizeof (BOOLEAN))) {
+    if (VariableData == TRUE) {
+      return;
+    }
+  }
+
+  for (Count = 0;
+       Count < sizeof (NonRemovableReversePriorityBootOrder) /
+               sizeof (NonRemovableReversePriorityBootOrder[0]);
+       Count++) {
+    SetMediaBootPriority (NonRemovableReversePriorityBootOrder[Count]);
+  }
+
+  for (Count = 0;
+       Count < sizeof (RemovableReversePriorityBootOrder) /
+               sizeof (RemovableReversePriorityBootOrder[0]);
+       Count++) {
+    SetMediaBootPriority (RemovableReversePriorityBootOrder[Count]);
+  }
+
+  SetAndroidBootPriority ();
+
+  VariableData = TRUE;
+  gRT->SetVariable (L"PlatformBootOrderSet", &gNVIDIATokenSpaceGuid,
+                    EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_NON_VOLATILE,
+                    sizeof (BOOLEAN), &VariableData);
+
+  return;
 }
