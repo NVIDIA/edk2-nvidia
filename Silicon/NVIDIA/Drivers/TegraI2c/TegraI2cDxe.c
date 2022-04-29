14f531e93ed94bf6f0493f28cbb4110026e27204
Jeff Brasen <jbrasen@nvidia.com>
2022-04-28 22:11:50 -0600

    feat: Add I2C support for the ssif-bmc node
    Add support for i2c enumeration for ssif-bmc devices
    
    Signed-off-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>

--- Silicon/NVIDIA/Drivers/TegraI2c/TegraI2cDxe.c
+++ Silicon/NVIDIA/Drivers/TegraI2c/TegraI2cDxe.c
@@ -1105,6 +1105,24 @@ TegraI2CDriverBindingStart (
         }
       }
     }
+    if (fdt_node_check_compatible (DeviceTreeNode->DeviceTreeBase,
+                                   I2cNodeOffset,
+                                   "ssif-bmc") == 0) {
+      Property = fdt_getprop (DeviceTreeNode->DeviceTreeBase, I2cNodeOffset, "reg", &PropertyLen);
+      if (Property != NULL && PropertyLen == sizeof (UINT32))  {
+        gBS->CopyMem (&I2cAddress, (VOID *) Property, PropertyLen);
+        I2cAddress = SwapBytes32 (I2cAddress);
+        DEBUG ((DEBUG_INFO, "%a: SSIF BMC Found.\n", __FUNCTION__));
+        DeviceGuid = &gNVIDIAI2cBmcSSIF;
+        Status = TegraI2cAddDevice (Private,
+                                    I2cAddress,
+                                    DeviceGuid,
+                                    fdt_get_phandle (DeviceTreeNode->DeviceTreeBase, I2cNodeOffset));
+        if (EFI_ERROR (Status)) {
+          goto ErrorExit;
+        }
+      }
+    }
   }
 
   Count = 0;
