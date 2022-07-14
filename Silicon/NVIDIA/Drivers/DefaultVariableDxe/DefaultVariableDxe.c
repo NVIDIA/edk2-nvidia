2e83fbb3f79462680e3431870ade44b83e918e1c
Girish Mahadevan <gmahadevan@nvidia.com>
2022-07-14 14:05:58 -0600

    feat: add support for efi system partition variables
    Add support to process variables that can be present on the EFI
    system partition.
    
    Lock the TnSpec variables TegraPlatformSpec and TegraPlatformCompatSpec
    to prevent OS modification after they are created.
    
    Signed-off-by: Girish Mahadevan <gmahadevan@nvidia.com>
    Tested-by: Bob Morgan <bobm@nvidia.com>
    Reviewed-by: Bob Morgan <bobm@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

--- Silicon/NVIDIA/Drivers/DefaultVariableDxe/DefaultVariableDxe.c
+++ Silicon/NVIDIA/Drivers/DefaultVariableDxe/DefaultVariableDxe.c
@@ -7,15 +7,21 @@
 **/
 
 #include <Uefi.h>
+#include <Guid/FileInfo.h>
 #include <Library/BaseLib.h>
 #include <Library/BaseMemoryLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 #include <Library/UefiRuntimeServicesTableLib.h>
 #include <Library/UefiLib.h>
 #include <Library/DebugLib.h>
+#include <Library/MemoryAllocationLib.h>
 #include <Library/DtPlatformDtbLoaderLib.h>
 #include <Library/DxeServicesLib.h>
 #include <Library/VariablePolicyHelperLib.h>
+#include <Library/FileHandleLib.h>
+#include <Library/DevicePathLib.h>
+#include <Library/PcdLib.h>
+#include <Protocol/PartitionInfo.h>
 #include <libfdt.h>
 
 #define VARIABLE_NODE_PATH    "/firmware/uefi/variables"
@@ -26,6 +32,11 @@
 #define VARIABLE_NV_PROP      "non-volatile"
 #define VARIABLE_LOCKED_PROP  "locked"
 #define VARIABLE_DATA_PROP    "data"
+#define ESP_VAR_DIR_PATH      L"EFI\\NVDA\\Variables"
+#define ESP_VAR_ATTR_EXP      (EFI_VARIABLE_NON_VOLATILE |\
+                               EFI_VARIABLE_BOOTSERVICE_ACCESS |\
+                               EFI_VARIABLE_RUNTIME_ACCESS)
+#define ESP_VAR_ATTR_SZ        (4)
 
 STATIC VOID     *Registration = NULL;
 STATIC VOID     *RegistrationPolicy = NULL;
@@ -67,7 +78,7 @@ LockVariable (
              VARIABLE_POLICY_TYPE_LOCK_NOW
              );
   if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "Failed to register lock policy\r\n"));
+    DEBUG ((DEBUG_ERROR, "Failed to register lock policy: %r\r\n", Status));
     return;
   }
 }
@@ -176,6 +187,311 @@ ProcessVariable (
   }
 }
 
+/**
+  Util Function to get the Variable Name and Guid read as a file name from the
+  EFI partition
+
+  @param FileInfo          File Handle of the Variable File.
+  @param VarName           Variable Name output.
+  @param EfiVarGuid        Guid of the Variable.
+
+**/
+STATIC
+EFI_STATUS
+GetEspVarNameAndGuid (
+  IN  EFI_FILE_INFO   *FileInfo,
+  OUT CHAR16          *VarName,
+  OUT EFI_GUID        *EfiVarGuid
+  )
+{
+  EFI_STATUS  Status;
+  CHAR16     *SplitPos;
+  CHAR16     *VarGuid;
+
+  SplitPos = StrStr (FileInfo->FileName, L"-");
+  if (SplitPos == NULL) {
+    DEBUG ((DEBUG_ERROR, "%a: Unexpected File Name %s",
+                         __FUNCTION__, FileInfo->FileName));
+    Status = EFI_INVALID_PARAMETER;
+    goto exit;
+  }
+  ZeroMem (VarName, VARIABLE_MAX_NAME);
+  Status = StrnCpyS (VarName,
+                     VARIABLE_MAX_NAME,
+                     FileInfo->FileName,
+                     ((SplitPos - FileInfo->FileName)));
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Strncpy Failed %r\n", __FUNCTION__, Status));
+    goto exit;
+  }
+  VarGuid = SplitPos + 1;
+  if ((StrToGuid (VarGuid, EfiVarGuid)) != RETURN_SUCCESS) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to convert %s to EFI_GUID\n",
+                          __FUNCTION__, VarGuid));
+    Status = EFI_INVALID_PARAMETER;
+    goto exit;
+  }
+exit:
+  return Status;
+}
+
+/**
+  Util Function to get the Variable Data, Size and Attributes from a
+  Variable File on the ESP partition.
+
+  @param VarFile          File Handle of the Variable File.
+  @param FileSize         Size of the File.
+  @param FileData         Output Buffer containing File Data.
+  @param VarData          Pointer to the Variable Data.
+  @param VarAttr          Variable attributes read from the file.
+  @param VarSize          Size of the Variable.
+
+**/
+STATIC
+EFI_STATUS
+GetEspVarDataAndAttr (
+  IN  EFI_FILE     *VarFile,
+  IN  UINTN        FileSize,
+  OUT VOID         **FileData,
+  OUT VOID         **VarData,
+  OUT UINT32       *VarAttr,
+  OUT UINTN        *VarSize
+  )
+{
+  EFI_STATUS    Status;
+  EFI_FILE_INFO *FileInfo;
+  CHAR8         *BytePtr;
+
+  FileInfo = FileHandleGetInfo (VarFile);
+  if (FileInfo == NULL) {
+    DEBUG ((DEBUG_ERROR, "%a: Invalid File Handle %r\n",
+                          __FUNCTION__, Status));
+    return Status;
+  }
+
+  *FileData = AllocateZeroPool (FileSize);
+  if (*FileData == NULL) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to allocate buffer for %s\r\n",
+                         __FUNCTION__, FileInfo->FileName));
+    return EFI_OUT_OF_RESOURCES;
+  }
+
+  FileHandleSetPosition(VarFile, 0);
+  Status = FileHandleRead (VarFile, &FileSize, *FileData);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to read File %s %r\n",
+                         __FUNCTION__, FileInfo->FileName, Status));
+    return Status;
+  }
+
+  BytePtr = *FileData;
+  CopyMem (VarAttr, BytePtr, ESP_VAR_ATTR_SZ);
+  BytePtr += 4;
+
+  *VarData = BytePtr;
+  *VarSize = FileSize - 4;
+  return Status;
+}
+
+/**
+  Process an EFI System Partition Variable
+
+  @param Dir          Pointer to the directory containing the variables..
+  @param FileInfo     File Info of the Variable File.
+
+**/
+
+STATIC
+EFI_STATUS
+ProcessEspVariable (
+  EFI_FILE       *Dir,
+  EFI_FILE_INFO  *FileInfo
+  )
+{
+  EFI_STATUS Status;
+  EFI_FILE   *File;
+  CHAR16     VarName[VARIABLE_MAX_NAME];
+  UINTN      VarSize;
+  EFI_GUID   EfiVarGuid;
+  UINT32     VarAttr;
+  VOID       *VarData;
+  UINT64     FileSize;
+  VOID       *FileData = NULL;
+  UINTN      ReadSize;
+
+  if ((FileInfo == NULL) || (Dir == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Status = Dir->Open (Dir,
+                      &File,
+                      FileInfo->FileName,
+                      (EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE),
+                      0);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to open File %s %r \n\r",
+                         __FUNCTION__, FileInfo->FileName,
+                         Status));
+    goto exit;
+  }
+
+  Status = GetEspVarNameAndGuid (FileInfo, VarName, &EfiVarGuid);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get Esp Var Name/Guid %r\n\r",
+                          __FUNCTION__, Status));
+    goto exit;
+  }
+
+  Status = FileHandleGetSize (File, &FileSize);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to get File Size %lu\n",
+                         __FUNCTION__, FileSize));
+    goto exit;
+  }
+
+  if ((FileSize > (PcdGet32 (PcdMaxVariableSize) + sizeof (VarAttr)))  ||
+      (FileSize < ESP_VAR_ATTR_SZ)) {
+    DEBUG ((DEBUG_ERROR, "%a: %s Invalid File Size %lu (min %u max %u)\r\n",
+                         __FUNCTION__, FileInfo->FileName, FileSize,
+                         ESP_VAR_ATTR_SZ,
+                         (PcdGet32 (PcdMaxVariableSize) + sizeof (VarAttr))));
+    Status = EFI_INVALID_PARAMETER;
+    goto exit;
+  }
+
+  ReadSize = (UINTN)FileSize;
+  Status = GetEspVarDataAndAttr (File,
+                                 ReadSize,
+                                 &FileData,
+                                 &VarData,
+                                 &VarAttr,
+                                 &VarSize);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: failed to read File Data %r\n\r",
+                         __FUNCTION__, Status));
+    goto exit;
+  }
+  if ((VarAttr & ESP_VAR_ATTR_EXP) != ESP_VAR_ATTR_EXP) {
+    DEBUG ((DEBUG_ERROR, "%a:Unexpected Var Attributes 0x%x (expected%x)\n",
+                          __FUNCTION__, VarAttr, ESP_VAR_ATTR_EXP));
+    Status = EFI_INVALID_PARAMETER;
+    goto exit;
+  }
+
+  if (VarSize == 0) {
+    VarData = NULL;
+  }
+
+  Status = gRT->SetVariable (VarName, &EfiVarGuid, VarAttr, VarSize, VarData);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to Set variable %s %r\r\n",
+                          __FUNCTION__, VarName, Status));
+    goto exit;
+  }
+exit:
+  if (FileHandleDelete (File) != EFI_SUCCESS) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to delete File %s\n",
+                         __FUNCTION__, FileInfo->FileName));
+  }
+  if (FileData != NULL) {
+    FreePool (FileData);
+  }
+  return Status;
+}
+
+/**
+  Locate the EFI System Partition Variables and Process them.
+
+*/
+STATIC
+EFI_STATUS
+GetAndProcessEspVariables (
+    VOID
+  )
+{
+  EFI_STATUS                       Status;
+  EFI_FILE_HANDLE                  DirHandle;
+  EFI_HANDLE                       EspDeviceHandle;
+  BOOLEAN                          NoFile;
+  EFI_FILE_INFO                    *FileInfo;
+  UINTN                            HandleSize;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL  *Fs;
+  EFI_FILE_HANDLE                  RootDir;
+
+  HandleSize = sizeof (EFI_HANDLE);
+  Status = gBS->LocateHandle (ByProtocol,
+                              &gEfiPartTypeSystemPartGuid,
+                              NULL,
+                              &HandleSize,
+                              &EspDeviceHandle);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "Failed to Locate System Partition Guid %r\n", Status));
+    return Status;
+  }
+
+  Status = gBS->HandleProtocol (EspDeviceHandle,
+                                &gEfiSimpleFileSystemProtocolGuid,
+                                (VOID **) &Fs);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "Failed to find FS %r\n", Status));
+    return Status;
+  }
+
+  Status = Fs->OpenVolume (Fs, &RootDir);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "Failed to open FS %r\n", Status));
+    return Status;
+  }
+
+  Status = RootDir->Open (
+                      RootDir,
+                      &DirHandle,
+                      ESP_VAR_DIR_PATH,
+                      EFI_FILE_MODE_READ,
+                      0);
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Can't find ESP Variables Dir %r\n",
+                          __FUNCTION__, Status));
+    RootDir->Close (RootDir);
+    return Status;
+  }
+
+  NoFile   = FALSE;
+  FileInfo = NULL;
+
+  for (Status = FileHandleFindFirstFile (DirHandle, &FileInfo);
+       !EFI_ERROR (Status) && !NoFile;
+       Status = FileHandleFindNextFile (DirHandle, FileInfo, &NoFile)) {
+    // Process each Variable
+    if ((StrnCmp (FileInfo->FileName, L".", VARIABLE_MAX_NAME) == 0) ||
+        (StrnCmp (FileInfo->FileName, L"..", VARIABLE_MAX_NAME) == 0)) {
+      continue;
+    }
+    Status = ProcessEspVariable (DirHandle, FileInfo);
+    if (EFI_ERROR (Status)) {
+      DEBUG ((DEBUG_ERROR, "%a: Failed to process %s\n",
+                            __FUNCTION__, FileInfo->FileName));
+      continue;
+    }
+  }
+
+  RootDir->Close (RootDir);
+  return Status;
+}
+
+/**
+  Update special variables
+
+**/
+STATIC
+VOID
+UpdateSpecialVariables (
+  VOID
+  )
+{
+  LockVariable (&gNVIDIAPublicVariableGuid, L"TegraPlatformSpec");
+  LockVariable (&gNVIDIAPublicVariableGuid, L"TegraPlatformCompatSpec");
+}
 
 /**
   Callback when variable protocol is ready.
@@ -225,15 +541,18 @@ VariableReady (
   }
   VariablesParsed = TRUE;
 
+  UpdateSpecialVariables ();
+
   Status = DtPlatformLoadDtb (&Dtb, &DtbSize);
   if (EFI_ERROR (Status)) {
     DEBUG ((DEBUG_ERROR, "Failed to get dtb - %r\r\n", Status));
-    return;
+    goto EspVar;
   }
 
   NodeOffset = fdt_path_offset (Dtb, VARIABLE_NODE_PATH);
   if (NodeOffset < 0) {
-    return;
+    DEBUG ((DEBUG_ERROR, "Failed to get Variable Node - %r\r\n", Status));
+    goto EspVar;
   }
 
   fdt_for_each_subnode (SubNodeOffset, Dtb, NodeOffset) {
@@ -271,10 +590,18 @@ VariableReady (
         }
         VariableGuid = &DtbGuid;
       }
-
       ProcessVariable (Dtb, VariableNodeOffset, VariableGuid);
     }
   }
+
+EspVar:
+  Status = GetAndProcessEspVariables ();
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to Process ESP Partition Variables %r\n",
+                         __FUNCTION__, Status));
+  }
+
+  UpdateSpecialVariables ();
 }
 
 /**
