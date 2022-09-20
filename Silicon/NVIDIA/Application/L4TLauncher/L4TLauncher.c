10e7b0285774c0eac982388860ac2e1c1a7ae880
Jake Garver <jake@nvidia.com>
2022-09-20 15:44:18 -0400

    style: Uncrustify L4TLauncher.c and PcieControllerDxe.c
    Signed-off-by: Jake Garver <jake@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

--- Silicon/NVIDIA/Application/L4TLauncher/L4TLauncher.c
+++ Silicon/NVIDIA/Application/L4TLauncher/L4TLauncher.c
@@ -1981,14 +1981,14 @@ ValidateRootfsStatus (
 
           // Rootfs slot is not linked with bootloader chain
           BootChainOverride = NonCurrentSlot;
-          DataSize = sizeof (BootChainOverride);
-          Status   = gRT->SetVariable (
-                    BOOT_OS_OVERRIDE_VARIABLE_NAME,
-                    &gNVIDIAPublicVariableGuid,
-                    EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS|EFI_VARIABLE_NON_VOLATILE,
-                    DataSize,
-                    &BootChainOverride
-                    );
+          DataSize          = sizeof (BootChainOverride);
+          Status            = gRT->SetVariable (
+                                     BOOT_OS_OVERRIDE_VARIABLE_NAME,
+                                     &gNVIDIAPublicVariableGuid,
+                                     EFI_VARIABLE_BOOTSERVICE_ACCESS|EFI_VARIABLE_RUNTIME_ACCESS|EFI_VARIABLE_NON_VOLATILE,
+                                     DataSize,
+                                     &BootChainOverride
+                                     );
           if (EFI_ERROR (Status)) {
             ErrorPrint (L"Failed to set OS override variable: %r\r\n", Status);
           }
@@ -2279,12 +2279,12 @@ BootAndroidStylePartition (
     }
 
     Status = DiskIo->ReadDisk (
-                        DiskIo,
-                        BlockIo->Media->MediaId,
-                        0,
-                        Size,
-                        KernelDtb
-                        );
+                       DiskIo,
+                       BlockIo->Media->MediaId,
+                       0,
+                       Size,
+                       KernelDtb
+                       );
     if (EFI_ERROR (Status)) {
       ErrorPrint (L"Failed to read disk\r\n");
       goto Exit;
