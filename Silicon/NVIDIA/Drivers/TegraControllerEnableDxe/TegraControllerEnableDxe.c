835c89ecef090d0849d01cf938af207d5ff31c99
kpilch <kpilch@nvidia.com>
2022-11-28 14:53:31 -0500

    feat: add T234 DLA ACPI device node
    Expose memory ranges and interrupts for each of the
    DLA instances in T234 under a single device node.
    
    Signed-off-by: Kamil Pilch <kpilch@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>

--- Silicon/NVIDIA/Drivers/TegraControllerEnableDxe/TegraControllerEnableDxe.c
+++ Silicon/NVIDIA/Drivers/TegraControllerEnableDxe/TegraControllerEnableDxe.c
@@ -15,6 +15,7 @@ NVIDIA_COMPATIBILITY_MAPPING  gDeviceCompatibilityMap[] = {
   { "nvidia,tegra30-hda",    &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
   { "nvidia,ga10b",          &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
   { "nvidia,th500-soc-hwpm", &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { "nvidia,tegra234-nvdla", &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
   { NULL,                    NULL                                        }
 };
 
