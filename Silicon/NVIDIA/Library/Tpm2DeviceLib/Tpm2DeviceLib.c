1f3e1b010aaa9af064f24ef090da706c25d90ccf
Jeff Brasen <jbrasen@nvidia.com>
2022-12-02 10:27:10 -0700

    chore: Fix missing copyrights
    File was forked from
    MdeModulePkg/Bus/Pci/SataControllerDxe/ComponentName.c
    But Intel copyright was dropped by mistake
    
    File was modularized from an arm fdf, re-adding copyright.
    
    Add missing copyright for Tpm2DeviceLib
    
    These files were originally inspired by the patterns used in
    OvmfPkg/PlatformCI/PlatformBuild.py.  Retain the copyright in that file.
    
    Signed-off-by: Jeff Brasen <jbrasen@nvidia.com>
    Signed-off-by: Dat Mach <dmach@nvidia.com>
    Signed-off-by: Jake Garver <jake@nvidia.com>

--- Silicon/NVIDIA/Library/Tpm2DeviceLib/Tpm2DeviceLib.c
+++ Silicon/NVIDIA/Library/Tpm2DeviceLib/Tpm2DeviceLib.c
@@ -2,6 +2,7 @@
 
   Copyright (c) 2022, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
+  Copyright (c) 2013 - 2018, Intel Corporation. All rights reserved. <BR>
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
