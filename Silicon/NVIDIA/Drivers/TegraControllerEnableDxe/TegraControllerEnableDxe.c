3e8fb57c1827fd87e7ff649805a4e00f75f12c3b
Jon Hunter <jonathanh@nvidia.com>
2023-03-14 12:36:54 +0000

    fix: Add Tegra194 HDA device-tree compatible string
    The latest upstream device-tree for Tegra194 removes the 'tegra30-hda'
    compatible string from the list of compatible strings for the Tegra194
    HDA device. This is causing various RAS errors to occur when UEFI boots
    and prevents Linux from booting. Fix this by adding the 'tegra194-hda'
    compatible string.
    
    Signed-off-by: Jon Hunter <jonathanh@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

--- Silicon/NVIDIA/Drivers/TegraControllerEnableDxe/TegraControllerEnableDxe.c
+++ Silicon/NVIDIA/Drivers/TegraControllerEnableDxe/TegraControllerEnableDxe.c
@@ -18,6 +18,7 @@
 NVIDIA_COMPATIBILITY_MAPPING  gDeviceCompatibilityMap[] = {
   { "nvidia,gv11b",           &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
   { "nvidia,tegra30-hda",     &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { "nvidia,tegra194-hda",    &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
   { "nvidia,ga10b",           &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
   { "nvidia,th500-soc-hwpm",  &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
   { "nvidia,tegra234-nvdla",  &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
