5b786bec0a1539c7ea4785d27df1ecde8ffaaa9e
Bob Morgan <bobm@nvidia.com>
2023-04-06 13:37:56 -0600

    fix: support USB FW loads from MM devices
    Change UsbFirmwareDxe to use NVIDIA_FW_IMAGE_PROTOCOL
    to load USB FW from either NS or MM devices on both
    T194 and T234 platforms.
    
    Enhance FwPartitionMmDxe to support reads larger than
    MM communication buffer size.
    
    Signed-off-by: Bob Morgan <bobm@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

--- Silicon/NVIDIA/Tegra/T194/Drivers/UsbFirmwareDxe/UsbSilFw.c
+++ Silicon/NVIDIA/Tegra/T194/Drivers/UsbFirmwareDxe/UsbSilFw.c
@@ -1,267 +1,53 @@
 /** @file
 
-  Copyright (c) 2020-2021, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+  Copyright (c) 2020-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 #include <PiDxe.h>
 
-#include <Library/TegraPlatformInfoLib.h>
 #include <Library/UefiBootServicesTableLib.h>
-#include <Library/UsbFirmwareLib.h>
 #include <Library/MemoryAllocationLib.h>
-#include <Protocol/BlockIo.h>
-#include <Protocol/NorFlash.h>
-#include <Uefi/UefiGpt.h>
 #include <Library/DebugLib.h>
-#include <Library/GptLib.h>
-#include <Library/BaseMemoryLib.h>
 #include <Library/BaseLib.h>
-#include <Library/DevicePathLib.h>
+#include <Library/FwImageLib.h>
+#include <Library/DtPlatformDtbLoaderLib.h>
+#include <libfdt.h>
 
 #include <Protocol/UsbFwProtocol.h>
 
+#define USB_FW_IMAGE_NAME  L"xusb-fw"
+
 NVIDIA_USBFW_PROTOCOL  mUsbFwData;
 
-#define GPT_PARTITION_BLOCK_SIZE  512
-
-/**
-  Read the data from the disk
-
-  @param[in]  Handle   Handle with storage protocol
-  @param[in]  Offset   Offset in the disk
-  @param[out] Buffer
-  @param[out] Size
-
-**/
 STATIC
-EFI_STATUS
+BOOLEAN
 EFIAPI
-ReadStorageData (
-  IN  EFI_HANDLE  Handle,
-  IN  UINT64      Offset,
-  OUT VOID        *Buffer,
-  IN  UINT64      Size
+UsbFirmwarePlatformIsSupported (
+  VOID
   )
 {
-  EFI_STATUS                 Status;
-  NVIDIA_NOR_FLASH_PROTOCOL  *NorFlashProtocol;
-  EFI_BLOCK_IO_PROTOCOL      *BlockIoProtocol;
-  VOID                       *TempBuffer;
-  UINT32                     BlockSize;
-  UINT64                     CopySize;
+  EFI_STATUS   Status;
+  VOID         *Dtb;
+  UINTN        DtbSize;
+  CONST CHAR8  *Model;
+  INT32        Length;
 
-  Status = gBS->HandleProtocol (Handle, &gNVIDIANorFlashProtocolGuid, (VOID **)&NorFlashProtocol);
-  if (!EFI_ERROR (Status)) {
-    Status = NorFlashProtocol->Read (
-                                 NorFlashProtocol,
-                                 Offset,
-                                 Size,
-                                 Buffer
-                                 );
-    return Status;
-  } else {
-    Status = gBS->HandleProtocol (Handle, &gEfiBlockIoProtocolGuid, (VOID **)&BlockIoProtocol);
-    if (!EFI_ERROR (Status)) {
-      BlockSize = BlockIoProtocol->Media->BlockSize;
-      if ((Offset % BlockSize) != 0) {
-        CopySize   = MIN (Size, BlockSize - (Offset % BlockSize));
-        TempBuffer = AllocatePool (BlockSize);
-        if (TempBuffer == NULL) {
-          DEBUG ((DEBUG_ERROR, "%a: Failed to allocate temp buffer\r\n", __FUNCTION__));
-          return EFI_OUT_OF_RESOURCES;
-        }
+  Status = DtPlatformLoadDtb (&Dtb, &DtbSize);
+  if (EFI_ERROR (Status)) {
+    return FALSE;
+  }
 
-        Status = BlockIoProtocol->ReadBlocks (
-                                    BlockIoProtocol,
-                                    BlockIoProtocol->Media->MediaId,
-                                    Offset / BlockSize,
-                                    BlockSize,
-                                    TempBuffer
-                                    );
-        if (EFI_ERROR (Status)) {
-          DEBUG ((DEBUG_ERROR, "%a: Failed to read data\r\n", __FUNCTION__));
-          FreePool (TempBuffer);
-          return Status;
-        }
-
-        CopyMem (Buffer, TempBuffer + (Offset % BlockSize), CopySize);
-        FreePool (TempBuffer);
-        Buffer += CopySize;
-        Offset += CopySize;
-        Size   -= CopySize;
-      }
-
-      CopySize = Size - (Size % BlockSize);
-      if (CopySize != 0) {
-        Status = BlockIoProtocol->ReadBlocks (
-                                    BlockIoProtocol,
-                                    BlockIoProtocol->Media->MediaId,
-                                    Offset / BlockSize,
-                                    CopySize,
-                                    Buffer
-                                    );
-        if (EFI_ERROR (Status)) {
-          DEBUG ((DEBUG_ERROR, "%a: Failed to read data\r\n", __FUNCTION__));
-          FreePool (TempBuffer);
-          return Status;
-        }
-
-        Buffer += CopySize;
-        Offset += CopySize;
-        Size   -= CopySize;
-      }
-
-      if (Size != 0) {
-        CopySize   = Size;
-        TempBuffer = AllocatePool (BlockSize);
-        if (TempBuffer == NULL) {
-          DEBUG ((DEBUG_ERROR, "%a: Failed to allocate temp buffer\r\n", __FUNCTION__));
-          return EFI_OUT_OF_RESOURCES;
-        }
-
-        Status = BlockIoProtocol->ReadBlocks (
-                                    BlockIoProtocol,
-                                    BlockIoProtocol->Media->MediaId,
-                                    Offset / BlockSize,
-                                    BlockSize,
-                                    TempBuffer
-                                    );
-        if (EFI_ERROR (Status)) {
-          DEBUG ((DEBUG_ERROR, "%a: Failed to read data\r\n", __FUNCTION__));
-          FreePool (TempBuffer);
-          return Status;
-        }
-
-        CopyMem (Buffer, TempBuffer, CopySize);
-        FreePool (TempBuffer);
-        Buffer += CopySize;
-        Offset += CopySize;
-        Size   -= CopySize;
-      }
-
-      return EFI_SUCCESS;
+  Model = (CONST CHAR8 *)fdt_getprop (Dtb, 0, "model", &Length);
+  if ((Length > 0) && (Model != NULL)) {
+    if (0 == AsciiStrCmp (Model, "e3360_1099")) {
+      DEBUG ((EFI_D_ERROR, "%a: Xavier-SLT unsupported\r\n", __FUNCTION__));
+      return FALSE;
     }
   }
 
-  DEBUG ((DEBUG_ERROR, "%a: Unable to read data\r\n", __FUNCTION__));
-  ASSERT (FALSE);
-  return EFI_DEVICE_ERROR;
-}
-
-/**
-  Read the last 512 bytes from the disk
-
-  @param[in]  Handle   Handle with storage protocol
-  @param[out] Partition
-
-**/
-STATIC
-EFI_STATUS
-EFIAPI
-ReadBackupGpt (
-  IN  EFI_HANDLE                  Handle,
-  OUT EFI_PARTITION_TABLE_HEADER  *PartitionHeader
-  )
-{
-  EFI_STATUS                 Status;
-  UINT64                     StorageSize;
-  NVIDIA_NOR_FLASH_PROTOCOL  *NorFlashProtocol;
-  NOR_FLASH_ATTRIBUTES       NorFlashAttributes;
-  EFI_BLOCK_IO_PROTOCOL      *BlockIoProtocol;
-
-  StorageSize = 0;
-  Status      = gBS->HandleProtocol (Handle, &gNVIDIANorFlashProtocolGuid, (VOID **)&NorFlashProtocol);
-  if (!EFI_ERROR (Status)) {
-    Status = NorFlashProtocol->GetAttributes (NorFlashProtocol, &NorFlashAttributes);
-    if (EFI_ERROR (Status)) {
-      DEBUG ((DEBUG_ERROR, "%a: Failed to get NOR Flash attributes (%r)\r\n", __FUNCTION__, Status));
-      return Status;
-    }
-
-    StorageSize = NorFlashAttributes.MemoryDensity;
-  } else {
-    Status = gBS->HandleProtocol (Handle, &gEfiBlockIoProtocolGuid, (VOID **)&BlockIoProtocol);
-    if (!EFI_ERROR (Status)) {
-      StorageSize = BlockIoProtocol->Media->BlockSize * (BlockIoProtocol->Media->LastBlock + 1);
-    }
-  }
-
-  if (StorageSize == 0) {
-    DEBUG ((DEBUG_ERROR, "%a: No storage detected\r\n", __FUNCTION__));
-    return EFI_DEVICE_ERROR;
-  }
-
-  return ReadStorageData (Handle, StorageSize - GPT_PARTITION_BLOCK_SIZE, PartitionHeader, sizeof (EFI_PARTITION_TABLE_HEADER));
-}
-
-/**
-  Check partition media to be valid
-
-  @param[in]  Handle   Handle with partition protocol
-
-**/
-STATIC
-EFI_STATUS
-EFIAPI
-CheckPartitionFlash (
-  IN EFI_HANDLE  Handle
-  )
-{
-  EFI_STATUS                Status;
-  EFI_DEVICE_PATH_PROTOCOL  *PartitionDevicePath;
-  EFI_DEVICE_PATH_PROTOCOL  *CurrentDevicePath;
-  BOOLEAN                   ValidFlash;
-
-  PartitionDevicePath = NULL;
-  CurrentDevicePath   = NULL;
-
-  // Query for Device Path on the handle
-  Status = gBS->HandleProtocol (
-                  Handle,
-                  &gEfiDevicePathProtocolGuid,
-                  (VOID **)&PartitionDevicePath
-                  );
-
-  if (EFI_ERROR (Status) || (PartitionDevicePath == NULL) || IsDevicePathEnd (PartitionDevicePath)) {
-    Status = EFI_UNSUPPORTED;
-    goto Exit;
-  }
-
-  // Check if any node on device path is of EMMC type
-  ValidFlash        = FALSE;
-  CurrentDevicePath = PartitionDevicePath;
-  while (IsDevicePathEnd (CurrentDevicePath) == FALSE) {
-    if ((CurrentDevicePath->Type == MESSAGING_DEVICE_PATH) &&
-        (CurrentDevicePath->SubType == MSG_EMMC_DP))
-    {
-      CurrentDevicePath = NextDevicePathNode (CurrentDevicePath);
-      if ((CurrentDevicePath->Type == HARDWARE_DEVICE_PATH) &&
-          (CurrentDevicePath->SubType == HW_CONTROLLER_DP))
-      {
-        CONTROLLER_DEVICE_PATH  *ControlNode = (CONTROLLER_DEVICE_PATH *)CurrentDevicePath;
-        if (ControlNode->ControllerNumber == 0) {
-          CurrentDevicePath = NextDevicePathNode (CurrentDevicePath);
-          if (IsDevicePathEnd (CurrentDevicePath)) {
-            ValidFlash = TRUE;
-          }
-        }
-      }
-
-      break;
-    }
-
-    CurrentDevicePath = NextDevicePathNode (CurrentDevicePath);
-  }
-
-  if (ValidFlash != TRUE) {
-    Status = EFI_UNSUPPORTED;
-    goto Exit;
-  }
-
-Exit:
-  return Status;
+  return TRUE;
 }
 
 /**
@@ -282,143 +68,55 @@ UsbFirmwareDxeInitialize (
   IN EFI_SYSTEM_TABLE  *SystemTable
   )
 {
-  UINTN                       ChipID;
-  EFI_STATUS                  Status;
-  UINTN                       NumOfHandles;
-  EFI_HANDLE                  *HandleBuffer = NULL;
-  EFI_HANDLE                  StorageHandle;
-  BOOLEAN                     StorageFound;
-  UINTN                       Index;
-  EFI_PARTITION_TABLE_HEADER  PartitionHeader;
-  VOID                        *PartitionEntryArray;
-  CONST EFI_PARTITION_ENTRY   *PartitionEntry;
-  CHAR8                       *UsbFwBuffer;
+  EFI_STATUS                Status;
+  CHAR8                     *UsbFwBuffer;
+  NVIDIA_FW_IMAGE_PROTOCOL  *FwImage;
+  FW_IMAGE_ATTRIBUTES       Attributes;
 
-  ChipID = TegraGetChipID ();
-  if (ChipID != T194_CHIP_ID) {
-    return EFI_SUCCESS;
+  if (!UsbFirmwarePlatformIsSupported ()) {
+    return EFI_UNSUPPORTED;
   }
 
-  StorageFound = FALSE;
-  Status       = gBS->LocateHandleBuffer (
-                        ByProtocol,
-                        &gNVIDIANorFlashProtocolGuid,
-                        NULL,
-                        &NumOfHandles,
-                        &HandleBuffer
-                        );
-  if (!EFI_ERROR (Status)) {
-    StorageHandle = HandleBuffer[0];
-    StorageFound  = TRUE;
-    FreePool (HandleBuffer);
-  } else {
-    Status = gBS->LocateHandleBuffer (
-                    ByProtocol,
-                    &gEfiBlockIoProtocolGuid,
-                    NULL,
-                    &NumOfHandles,
-                    &HandleBuffer
-                    );
-    if (!EFI_ERROR (Status)) {
-      for (Index = 0; Index < NumOfHandles; Index++) {
-        Status = CheckPartitionFlash (HandleBuffer[Index]);
-        if (!EFI_ERROR (Status)) {
-          StorageHandle = HandleBuffer[Index];
-          StorageFound  = TRUE;
-          break;
-        }
-      }
-
-      FreePool (HandleBuffer);
-    }
-  }
-
-  if (!StorageFound) {
-    DEBUG ((DEBUG_ERROR, "%a: No storage partition\r\n", __FUNCTION__));
-    ASSERT (FALSE);
+  FwImage = FwImageFindProtocol (USB_FW_IMAGE_NAME);
+  if (FwImage == NULL) {
+    DEBUG ((DEBUG_ERROR, "USB FW image %s not found\r\n", USB_FW_IMAGE_NAME));
     return EFI_NOT_FOUND;
   }
 
-  Status = ReadBackupGpt (StorageHandle, &PartitionHeader);
+  Status = FwImage->GetAttributes (FwImage, &Attributes);
   if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "Error Reading GPT Header\n"));
-    return EFI_DEVICE_ERROR;
-  }
-
-  Status = GptValidateHeader (&PartitionHeader);
-  if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "Invalid efi partition table header\n"));
-    return EFI_DEVICE_ERROR;
-  }
-
-  //
-  // Read the partition Entries;
-  //
-  PartitionEntryArray = AllocateZeroPool (GptPartitionTableSizeInBytes (&PartitionHeader));
-  if (PartitionEntryArray == NULL) {
-    return EFI_OUT_OF_RESOURCES;
-  }
-
-  Status = ReadStorageData (
-             StorageHandle,
-             PartitionHeader.PartitionEntryLBA * GPT_PARTITION_BLOCK_SIZE,
-             PartitionEntryArray,
-             GptPartitionTableSizeInBytes (&PartitionHeader)
-             );
-  if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "%a: Failed to read GPT partition array (%r)\r\n", __FUNCTION__, Status));
-    FreePool (PartitionEntryArray);
+    DEBUG ((DEBUG_ERROR, "Failed to get %s attributes\r\n", USB_FW_IMAGE_NAME));
     return Status;
   }
 
-  Status = GptValidatePartitionTable (&PartitionHeader, PartitionEntryArray);
+  mUsbFwData.UsbFwSize = Attributes.Bytes;
+  UsbFwBuffer          = AllocateZeroPool (mUsbFwData.UsbFwSize);
+  mUsbFwData.UsbFwBase = UsbFwBuffer;
+  Status               = FwImage->Read (FwImage, 0, mUsbFwData.UsbFwSize, mUsbFwData.UsbFwBase, FW_IMAGE_RW_FLAG_NONE);
   if (EFI_ERROR (Status)) {
-    DEBUG ((DEBUG_ERROR, "Invalid PartitionEntryArray\r\n"));
-    FreePool (PartitionEntryArray);
+    DEBUG ((DEBUG_ERROR, "Failed to read Partition\r\n"));
     return Status;
   }
 
-  // Find variable and FTW partitions
-  PartitionEntry = GptFindPartitionByName (
-                     &PartitionHeader,
-                     PartitionEntryArray,
-                     L"xusb-fw"
-                     );
-  if (PartitionEntry != NULL) {
-    mUsbFwData.UsbFwSize = GptPartitionSizeInBlocks (PartitionEntry) * GPT_PARTITION_BLOCK_SIZE;
-    UsbFwBuffer          = AllocateZeroPool (mUsbFwData.UsbFwSize);
-    mUsbFwData.UsbFwBase = UsbFwBuffer;
-    Status               = ReadStorageData (
-                             StorageHandle,
-                             PartitionEntry->StartingLBA * GPT_PARTITION_BLOCK_SIZE,
-                             mUsbFwData.UsbFwBase,
-                             mUsbFwData.UsbFwSize
-                             );
-    if (EFI_ERROR (Status)) {
-      DEBUG ((DEBUG_ERROR, "Failed to read Partition\r\n"));
-    } else {
-      if (0 == AsciiStrnCmp (
-                 (CONST CHAR8 *)mUsbFwData.UsbFwBase,
-                 (CONST CHAR8 *)PcdGetPtr (PcdSignedImageHeaderSignature),
-                 sizeof (UINT32)
-                 ))
-      {
-        mUsbFwData.UsbFwSize -= PcdGet32 (PcdSignedImageHeaderSize);
-        mUsbFwData.UsbFwBase  = UsbFwBuffer + PcdGet32 (PcdSignedImageHeaderSize);
-      }
-
-      Status = gBS->InstallMultipleProtocolInterfaces (
-                      &ImageHandle,
-                      &gNVIDIAUsbFwProtocolGuid,
-                      (VOID *)&mUsbFwData,
-                      NULL
-                      );
-      if (EFI_ERROR (Status)) {
-        DEBUG ((DEBUG_ERROR, "%a: Failed to install USB firmware protocol - %r\r\n", __FUNCTION__, Status));
-      }
-    }
+  if (0 == AsciiStrnCmp (
+             (CONST CHAR8 *)mUsbFwData.UsbFwBase,
+             (CONST CHAR8 *)PcdGetPtr (PcdSignedImageHeaderSignature),
+             sizeof (UINT32)
+             ))
+  {
+    mUsbFwData.UsbFwSize -= PcdGet32 (PcdSignedImageHeaderSize);
+    mUsbFwData.UsbFwBase  = UsbFwBuffer + PcdGet32 (PcdSignedImageHeaderSize);
   }
 
-  FreePool (PartitionEntryArray);
+  Status = gBS->InstallMultipleProtocolInterfaces (
+                  &ImageHandle,
+                  &gNVIDIAUsbFwProtocolGuid,
+                  (VOID *)&mUsbFwData,
+                  NULL
+                  );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((DEBUG_ERROR, "%a: Failed to install USB firmware protocol - %r\r\n", __FUNCTION__, Status));
+  }
+
   return Status;
 }
