ffc2697ee5f369e8746671dced75727bad5d51f2
Jeff Brasen <jbrasen@nvidia.com>
2023-05-05 14:41:58 +0000

    feat: Enable Threading driver
    Code cleanup and remove intel specific behavior
    
    Signed-off-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>

--- Silicon/NVIDIA/Include/Protocol/Threading.h
+++ Silicon/NVIDIA/Include/Protocol/Threading.h
@@ -10,8 +10,8 @@
  *
  */
 
-#ifndef MDEMODULEPKG_INCLUDE_PROTOCOL_THREADING_H_
-#define MDEMODULEPKG_INCLUDE_PROTOCOL_THREADING_H_
+#ifndef THREADING_H_
+#define THREADING_H_
 
 //
 // Threading Protocol GUID value
@@ -32,35 +32,14 @@ VOID
   IN VOID   *Argument
   );
 
-// typedef struct _CPU_CHECKPOINT {
-//  CHAR8           *File;
-//  UINTN           Line;
-//  UINTN           Timestamp;
-// } CPU_CHECKPOINT;
-
 typedef VOID *EFI_THREAD;
 
 typedef
 EFI_STATUS
 (EFIAPI *EFI_THREADING_IDENTIFY_CPU)(
-  OUT UINT32    *CpuId,
+  OUT UINTN     *CpuId,
   OUT BOOLEAN   *IsBsp
   );
-
-// typedef
-// EFI_STATUS
-// (EFIAPI *EFI_THREADING_GET_CPU_CHECKPOINTS)(
-//  IN      UINT32          CpuId,
-//  IN OUT  CPU_CHECKPOINT  **Checkpoints,
-//  IN OUT  UINTN           *Length
-//  );
-//
-// typedef
-// VOID
-// (EFIAPI *EFI_THREADING_REGISTER_CPU_CHECKPOINT)(
-//  IN  CHAR8*            File,
-//  IN  UINTN             Line
-//  );
 
 typedef
 EFI_STATUS
@@ -76,13 +55,13 @@ EFI_STATUS
 typedef
 EFI_STATUS
 (EFIAPI *EFI_THREADING_WAIT_FOR_THREAD)(
-  IN  EFI_THREAD      *Thread
+  IN  EFI_THREAD      Thread
   );
 
 typedef
 EFI_STATUS
 (EFIAPI *EFI_THREADING_CLEANUP_THREAD)(
-  IN  EFI_THREAD      *Thread
+  IN  EFI_THREAD      Thread
   );
 
 typedef
@@ -95,13 +74,11 @@ EFI_STATUS
 typedef
 EFI_STATUS
 (EFIAPI *EFI_THREADING_ABORT_THREAD)(
-  IN  EFI_THREAD      *Thread
+  IN  EFI_THREAD      Thread
   );
 
 struct _EFI_THREADING_PROTOCOL {
   EFI_THREADING_IDENTIFY_CPU       IdentifyCpu;
-  //  EFI_THREADING_GET_CPU_CHECKPOINTS       GetCpuCheckpoints;
-  //  EFI_THREADING_REGISTER_CPU_CHECKPOINT   RegisterCpuCheckpoint;
   EFI_THREADING_SPAWN_THREAD       SpawnThread;
   EFI_THREADING_WAIT_FOR_THREAD    WaitForThread;
   EFI_THREADING_CLEANUP_THREAD     CleanupThread;
