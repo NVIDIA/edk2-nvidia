b76030f599cdbc85b30f8769277f591cfa822f7f
Nickle Wang <nicklew@nvidia.com>
2023-06-30 21:15:01 +0800

    chore: implement platform Redfish override protocol
    Implement platform Redfish override protocol and mark existing
    boot chain as good when Redfish is about to reboot system.
    
    Signed-off-by: Nickle Wang <nicklew@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>
    Tested-by: Nick Ramirez <nramirez@nvidia.com>

---
+++ Silicon/NVIDIA/Drivers/PlatformRedfishOverrideDxe/PlatformRedfishOverrideDxe.h
@@ -0,0 +1,23 @@
+/** @file
+  Platform Redfish override driver header file.
+
+  Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef PLATFORM_REDFISH_OVERRIDE_DXE_H_
+#define PLATFORM_REDFISH_OVERRIDE_DXE_H_
+
+#include <Uefi.h>
+
+#include <Library/BaseLib.h>
+#include <Library/UefiLib.h>
+#include <Library/DebugLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/PlatformResourceLib.h>
+
+#include <Protocol/EdkIIRedfishOverrideProtocol.h>
+
+#endif
