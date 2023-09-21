10d53bc2ba2fa3898656ec909fbeb0529b5c5dfa
Jake Garver <jake@nvidia.com>
2023-09-21 13:37:16 +0000

    chore: Add DeviceTreeHelperStubLib
    This is a stub lib for use during host based testing.  At this stage,
    it's just roughed in and empty.  It will be necessary for upcoming
    support of AndroidBootDxe in the TegraVirt platform.
    
    Signed-off-by: Jake Garver <jake@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

---
+++ Silicon/NVIDIA/Library/HostBasedTestStubLib/DeviceTreeHelperStubLib/DeviceTreeHelperStubLib.c
@@ -0,0 +1,11 @@
+/** @file
+
+  DeviceTreeHelperLib stubs for host based tests
+
+  SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <HostBasedTestStubLib/DeviceTreeHelperStubLib.h>
