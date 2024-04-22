6efdba91db02b093fd421d642ffca77f77910997
Jeff Brasen <jbrasen@nvidia.com>
2024-04-22 08:06:49 -0700

    fix: Delay iSCSI till after ACPI
    If ACPI is availble on the system then make sure
    iSCSI driver is loader after this as connection will
    only create iBFT table if ACPI tables are installed.
    
    Signed-off-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

---
+++ Silicon/NVIDIA/Library/PlatformHasConfigManagerLib/PlatformHasConfigManagerLib.c
@@ -0,0 +1,18 @@
+/** @file
+  Library that adds depex on ConfigurationManager
+
+  SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+**/
+
+#include <Uefi.h>
+
+EFI_STATUS
+EFIAPI
+PlatformHasConfigManagerInitialize (
+  VOID
+  )
+{
+  return EFI_SUCCESS;
+}
