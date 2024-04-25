c7302db2813babe7e5e49a5f3095cfeade8c5b32
Ashish Singhal <ashishsingha@nvidia.com>
2024-04-25 17:41:09 +0000

    fix(fastboot): fix upstream driver dependency
    upstream fastboot usb transport needs dependency on bsp
    usb driver.
    
    Signed-off-by: Ashish Singhal <ashishsingha@nvidia.com>

---
+++ Silicon/NVIDIA/Library/TegraUsbNullLib/TegraUsbNullLib.c
@@ -0,0 +1,20 @@
+/** @file
+
+  Tegra USB Null Library
+
+  SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#include <Uefi/UefiBaseType.h>
+
+EFI_STATUS
+EFIAPI
+TegraUsbNullLibConstructor (
+  VOID
+  )
+{
+  return EFI_SUCCESS;
+}
