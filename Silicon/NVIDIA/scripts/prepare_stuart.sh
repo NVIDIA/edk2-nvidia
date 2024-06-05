3a8255f335cd64fa9904690810a27d603a1d955b
Jake Garver <jake@nvidia.com>
2024-06-05 16:22:25 -0700

    feat(build): Allow prepare steps to be skipped
    Introduce new env vars that can affect the prepare steps called by
    build.sh scripts:
    - UEFI_SKIP_VENV.  This will skip creation and use of a python
      virtualenv. The required python modules must already be installed in
      the build environment.
    - UEFI_SKIP_UPDATE.  This will skip stuart_update. The required
      dependencies must already be installed in the build environment.
    
    Signed-off-by: Jake Garver <jake@nvidia.com>
    Reviewed-by: Nick Ramirez <nramirez@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

--- Silicon/NVIDIA/scripts/prepare_stuart.sh
+++ Silicon/NVIDIA/scripts/prepare_stuart.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021-2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2021-2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 
 
 ###############################################################################
 # Prepares the current workspace to build NVIDIA UEFI images using the stuart
-# build system.  Assumes the workspace has been created using submodules.
+# build system.
 
 # This script, along with others in this directory, serves as a working example
 # of building NVIDIA UEFI images.
@@ -22,23 +22,27 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 . ${SCRIPT_DIR}/setenv_stuart.sh
 
 
-# Create a python virtual env, if we haven't already.
-if [ ! -e venv/bin/activate ]; then
-  _msg "Creating Python virtual environment in `pwd`/venv..."
-  virtualenv -p python3 venv
-  . venv/bin/activate
-  _msg "Installing required Python packages..."
-  pip install --upgrade -r edk2/pip-requirements.txt
-  pip install --upgrade kconfiglib
-else
-  _msg "Activating Python virtual environment."
-  . venv/bin/activate
+if [[ -z "${UEFI_SKIP_VENV}" ]]; then
+  # Create a python virtual env, if we haven't already.
+  if [ ! -e venv/bin/activate ]; then
+    _msg "Creating Python virtual environment in `pwd`/venv..."
+    virtualenv -p python3 venv
+    . venv/bin/activate
+    _msg "Installing required Python packages..."
+    pip install --upgrade -r edk2/pip-requirements.txt
+    pip install --upgrade kconfiglib
+  else
+    _msg "Activating Python virtual environment."
+    . venv/bin/activate
+  fi
 fi
 
-_msg "Updating build environment (${PLATFORM_BUILD})."
-# Requires mono to be installed following the instructions here:
-# - https://github.com/tianocore/edk2-pytool-extensions/blob/master/docs/usability/using_extdep.md
-stuart_update ${STUART_UPDATE_OPTIONS} -c ${PLATFORM_BUILD}
+if [[ -z "${UEFI_SKIP_UPDATE}" ]]; then
+  _msg "Updating build environment (${PLATFORM_BUILD})."
+  # Requires mono to be installed following the instructions here:
+  # - https://github.com/tianocore/edk2-pytool-extensions/blob/master/docs/usability/using_extdep.md
+  stuart_update ${STUART_UPDATE_OPTIONS} -c ${PLATFORM_BUILD}
+fi
 
 _msg "Building basetools."
 python edk2/BaseTools/Edk2ToolsBuild.py -t GCC5
