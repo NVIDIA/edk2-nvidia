1d66cd66d373ddad7bee6b4da56a2f17348ed8b6
Dat Mach <dmach@nvidia.com>
2024-07-30 17:48:53 -0700

    fix: Retry if get version fails
    Get version from EROT may fail on the initial boot after flashing.
    Adding retry to mitigate the issue.
    
    Signed-off-by: Dat Mach <dmach@nvidia.com>
    Reviewed-by: Bob Morgan <bobm@nvidia.com>
    Reviewed-by: Nick Ramirez <nramirez@nvidia.com>

--- Silicon/NVIDIA/Library/FmpErotLib/FmpErotSupport.c
+++ Silicon/NVIDIA/Library/FmpErotLib/FmpErotSupport.c
@@ -2,7 +2,7 @@
 
   FMP erot support functions
 
-  Copyright (c) 2022-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+  SPDX-FileCopyrightText: Copyright (c) 2022 - 2024 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -566,7 +566,11 @@ FmpErotEndOfDxeNotify (
 
   Status = FmpErotGetVersionInfo ();
   if (EFI_ERROR (Status)) {
-    goto Done;
+    // Retry once if fails
+    Status = FmpErotGetVersionInfo ();
+    if (EFI_ERROR (Status)) {
+      goto Done;
+    }
   }
 
   if (mInstaller == NULL) {
