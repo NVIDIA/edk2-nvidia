3b8ff503ff73d2fd5e4fa3727ae977db6dd9e802
Ashish Singhal <ashishsingha@nvidia.com>
2024-09-30 21:14:14 -0600

    fix: add redfish to server igx config
    add redfish client package to server igx  package list as
    there are cases for this on igx.
    
    Signed-off-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Girish Mahadevan <gmahadevan@nvidia.com>
    Reviewed-by: Jake Garver <jake@nvidia.com>

--- Platform/NVIDIA/ServerIgx/PlatformBuild.py
+++ Platform/NVIDIA/ServerIgx/PlatformBuild.py
@@ -19,6 +19,11 @@ class ServerIgxSettingsManager(NVIDIASettingsManager):
     def GetGuid(self):
         return "0927650c-5959-4dfb-a40f-0f05a3cefbd6"
 
+    def GetPackagesPath(self):
+        return super().GetPackagesPath() + [
+            "edk2-redfish-client/"
+        ]
+
     def GetFirmwareVolume(self):
         return "FV/UEFI_NS.Fv"
 
