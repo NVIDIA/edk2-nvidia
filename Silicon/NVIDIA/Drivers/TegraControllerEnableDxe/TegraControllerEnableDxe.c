3f8b494e6541f487e54f3ba0571c04514a637e9c
Bob Morgan <bobm@nvidia.com>
2025-02-10 16:02:37 -0800

    chore(t264): enable host1x support
    Enable T264 host1x support.
    
    Signed-off-by: Bob Morgan <bobm@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>

--- Silicon/NVIDIA/Drivers/TegraControllerEnableDxe/TegraControllerEnableDxe.c
+++ Silicon/NVIDIA/Drivers/TegraControllerEnableDxe/TegraControllerEnableDxe.c
@@ -2,7 +2,7 @@
 
   Tegra Controller Enable Driver
 
-  SPDX-FileCopyrightText: Copyright (c) 2020-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+  SPDX-FileCopyrightText: Copyright (c) 2020-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -16,15 +16,14 @@
 #define HWPM_LA_MAX_CLOCK   625000000
 
 NVIDIA_COMPATIBILITY_MAPPING  gDeviceCompatibilityMap[] = {
-  { "nvidia,gv11b",           &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
-  { "nvidia,*-hda",           &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
-  { "nvidia,ga10b",           &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
-  { "nvidia,*-soc-hwpm",      &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
-  { "nvidia,*-nvdla",         &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
-  { "nvidia,tegra264-host1x", NULL                                        },
-  { "nvidia,*-host1x",        &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
-  { "nvidia,*-rce",           &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
-  { NULL,                     NULL                                        }
+  { "nvidia,gv11b",      &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { "nvidia,*-hda",      &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { "nvidia,ga10b",      &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { "nvidia,*-soc-hwpm", &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { "nvidia,*-nvdla",    &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { "nvidia,*-host1x",   &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { "nvidia,*-rce",      &gNVIDIANonDiscoverableEnableOnlyDeviceGuid },
+  { NULL,                NULL                                        }
 };
 
 NVIDIA_DEVICE_DISCOVERY_CONFIG  gDeviceDiscoverDriverConfig = {
