cceb122c10da3c682157a23721dae35ec4c9bcdf
Ashish Singhal <ashishsingha@nvidia.com>
2025-04-03 10:59:23 -0600

    chore: update smmu protocol interfaces
    update smmu protocol interfaces to reflect only what
    is intended to be done by the smmu driver.
    
    Signed-off-by: Swatisri Kantamsetti <swatisrik@nvidia.com>
    Tested-by: Ashish Singhal <ashishsingha@nvidia.com>
    Reviewed-by: Ashish Singhal <ashishsingha@nvidia.com>

--- Silicon/NVIDIA/Drivers/SmmuV3Dxe/SmmuV3Dxe.c
+++ Silicon/NVIDIA/Drivers/SmmuV3Dxe/SmmuV3Dxe.c
@@ -1519,103 +1519,29 @@ InitializeSmmuV3 (
 }
 
 /**
-  Set IOMMU attribute for a system memory.
-
-  If the IOMMU protocol exists, the system memory cannot be used
-  for DMA by default.
-
-  When a device requests a DMA access for a system memory,
-  the device driver need use SetAttribute() to update the IOMMU
-  attribute to request DMA access (read and/or write).
-
-  The DeviceHandle is used to identify which device submits the request.
-  The IOMMU implementation need translate the device path to an IOMMU device ID,
-  and set IOMMU hardware register accordingly.
-  1) DeviceHandle can be a standard PCI device.
-     The memory for BusMasterRead need set EDKII_IOMMU_ACCESS_READ.
-     The memory for BusMasterWrite need set EDKII_IOMMU_ACCESS_WRITE.
-     The memory for BusMasterCommonBuffer need set EDKII_IOMMU_ACCESS_READ|EDKII_IOMMU_ACCESS_WRITE.
-     After the memory is used, the memory need set 0 to keep it being protected.
-  2) DeviceHandle can be an ACPI device (ISA, I2C, SPI, etc).
-     The memory for DMA access need set EDKII_IOMMU_ACCESS_READ and/or EDKII_IOMMU_ACCESS_WRITE.
+  Set SMMU attribute for a system memory.
 
   @param[in]  This              The protocol instance pointer.
-  @param[in]  DeviceHandle      The device who initiates the DMA access request.
   @param[in]  Mapping           The mapping value returned from Map().
   @param[in]  IoMmuAccess       The IOMMU access.
+  @param[in]  Rid               The requester ID.
+  @param[in]  Segment           The segment ID.
 
   @retval EFI_SUCCESS            The IoMmuAccess is set for the memory range specified by DeviceAddress and Length.
-  @retval EFI_INVALID_PARAMETER  DeviceHandle is an invalid handle.
-  @retval EFI_INVALID_PARAMETER  Mapping is not a value that was returned by Map().
-  @retval EFI_INVALID_PARAMETER  IoMmuAccess specified an illegal combination of access.
-  @retval EFI_UNSUPPORTED        DeviceHandle is unknown by the IOMMU.
-  @retval EFI_UNSUPPORTED        The bit mask of IoMmuAccess is not supported by the IOMMU.
-  @retval EFI_UNSUPPORTED        The IOMMU does not support the memory range specified by Mapping.
+  @retval EFI_INVALID_PARAMETER  Invalid Input Parameters.
+  @retval EFI_UNSUPPORTED        The IoMmuAccess or the Mapping is not supported by the SMMU.
   @retval EFI_OUT_OF_RESOURCES   There are not enough resources available to modify the IOMMU access.
-  @retval EFI_DEVICE_ERROR       The IOMMU device reported an error while attempting the operation.
+  @retval EFI_DEVICE_ERROR       The SMMU device reported an error while attempting the operation.
 
 **/
 EFI_STATUS
 EFIAPI
 SetAttributeSmmuV3 (
-  IN EDKII_IOMMU_PROTOCOL  *This,
-  IN EFI_HANDLE            DeviceHandle,
-  IN VOID                  *Mapping,
-  IN UINT64                IoMmuAccess
-  )
-{
-  return EFI_DEVICE_ERROR;
-}
-
-/**
-  Provides the controller-specific addresses required to access system memory from a
-  DMA bus master.
-
-  @param  This                  The protocol instance pointer.
-  @param  Operation             Indicates if the bus master is going to read or write to system memory.
-  @param  HostAddress           The system memory address to map to the PCI controller.
-  @param  NumberOfBytes         On input the number of bytes to map. On output the number of bytes
-                                that were mapped.
-  @param  DeviceAddress         The resulting map address for the bus master PCI controller to use to
-                                access the hosts HostAddress.
-  @param  Mapping               A resulting value to pass to Unmap().
-
-  @retval EFI_SUCCESS           The range was mapped for the returned NumberOfBytes.
-  @retval EFI_UNSUPPORTED       The HostAddress cannot be mapped as a common buffer.
-  @retval EFI_INVALID_PARAMETER One or more parameters are invalid.
-  @retval EFI_OUT_OF_RESOURCES  The request could not be completed due to a lack of resources.
-  @retval EFI_DEVICE_ERROR      The system hardware could not map the requested address.
-
-**/
-EFI_STATUS
-EFIAPI
-MapSmmuV3 (
-  IN     EDKII_IOMMU_PROTOCOL   *This,
-  IN     EDKII_IOMMU_OPERATION  Operation,
-  IN     VOID                   *HostAddress,
-  IN OUT UINTN                  *NumberOfBytes,
-  OUT    EFI_PHYSICAL_ADDRESS   *DeviceAddress,
-  OUT    VOID                   **Mapping
-  )
-{
-  return EFI_DEVICE_ERROR;
-}
-
-/**
-  Completes the Map() operation and releases any corresponding resources.
-
-  @param  This                  The protocol instance pointer.
-  @param  Mapping               The mapping value returned from Map().
-
-  @retval EFI_SUCCESS           The range was unmapped.
-  @retval EFI_INVALID_PARAMETER Mapping is not a value that was returned by Map().
-  @retval EFI_DEVICE_ERROR      The data was not committed to the target system memory.
-**/
-EFI_STATUS
-EFIAPI
-UnmapSmmuV3 (
-  IN  EDKII_IOMMU_PROTOCOL  *This,
-  IN  VOID                  *Mapping
+  IN NVIDIA_SMMUV3_CONTROLLER_PROTOCOL  *This,
+  IN VOID                               *Mapping,
+  IN UINT64                             IoMmuAccess,
+  IN UINT16                             Rid,
+  IN UINT16                             Segment
   )
 {
   return EFI_DEVICE_ERROR;
@@ -1831,6 +1757,8 @@ DeviceDiscoveryNotify (
         DEBUG ((DEBUG_ERROR, "%a: Unable to get phandle for node\n", __FUNCTION__));
         goto Exit;
       }
+
+      Private->SmmuV3ControllerProtocol.SetAttribute = SetAttributeSmmuV3;
 
       DEBUG ((DEBUG_ERROR, "%a: Base Addr 0x%lx\n", __FUNCTION__, Private->BaseAddress));
       DEBUG ((DEBUG_ERROR, "%a: PHandle 0x%lx\n", __FUNCTION__, Private->SmmuV3ControllerProtocol.PHandle));
--- Silicon/NVIDIA/Include/Protocol/SmmuV3Protocol.h
+++ Silicon/NVIDIA/Include/Protocol/SmmuV3Protocol.h
@@ -10,8 +10,6 @@
 #ifndef __NVIDIA_SMMU_V3_PROTOCOL_H__
 #define __NVIDIA_SMMU_V3_PROTOCOL_H__
 
-#include <Protocol/IoMmu.h>
-
 #define NVIDIA_SMMUV3_CONTROLLER_PROTOCOL_GUID \
   { \
   0xF6C64F84, 0x702C, 0x4BE7, { 0xA4, 0x1B, 0x64, 0xD5, 0xB5, 0x5F, 0x10, 0x1C } \
@@ -22,12 +20,36 @@
 //
 typedef struct _NVIDIA_SMMUV3_CONTROLLER_PROTOCOL NVIDIA_SMMUV3_CONTROLLER_PROTOCOL;
 
+/**
+  Set SMMU attribute for a system memory.
+
+  @param[in]  This              The protocol instance pointer.
+  @param[in]  Mapping           The mapping value returned from Map().
+  @param[in]  IoMmuAccess       The IOMMU access.
+  @param[in]  Rid               The requester ID.
+  @param[in]  Segment           The segment ID.
+
+  @retval EFI_SUCCESS            The IoMmuAccess is set for the memory range specified by DeviceAddress and Length.
+  @retval EFI_INVALID_PARAMETER  Invalid Input Parameters.
+  @retval EFI_UNSUPPORTED        The IoMmuAccess or the Mapping is not supported by the SMMU.
+  @retval EFI_OUT_OF_RESOURCES   There are not enough resources available to modify the IOMMU access.
+  @retval EFI_DEVICE_ERROR       The SMMU device reported an error while attempting the operation.
+
+**/
+typedef
+EFI_STATUS
+(EFIAPI *NVIDIA_SMMU_SET_ATTRIBUTE)(
+  IN NVIDIA_SMMUV3_CONTROLLER_PROTOCOL  *This,
+  IN VOID                               *Mapping,
+  IN UINT64                             IoMmuAccess,
+  IN UINT16                             Rid,
+  IN UINT16                             Segment
+  );
+
 /// NVIDIA_SMMUV3_CONTROLLER_PROTOCOL protocol structure.
 struct _NVIDIA_SMMUV3_CONTROLLER_PROTOCOL {
   UINT32                       PHandle;
-  EDKII_IOMMU_SET_ATTRIBUTE    SetAttribute;
-  EDKII_IOMMU_MAP              Map;
-  EDKII_IOMMU_UNMAP            Unmap;
+  NVIDIA_SMMU_SET_ATTRIBUTE    SetAttribute;
 };
 
 extern EFI_GUID  gNVIDIASmmuV3ProtocolGuid;
