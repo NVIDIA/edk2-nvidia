1493a225859138b342b0c8b3cfe78708a47c727a
ley <ley@nvidia.com>
2025-05-28 15:12:56 +0000

    feat: add a new property to set fan pwm
    Add a new property "pulse-width" to support pulse width setting of high
    level for fan pwm.
    
    Signed-off-by: ley <ley@nvidia.com>
    Tested-by: Jake Garver <jake@nvidia.com>
    Reviewed-by: Jake Garver <jake@nvidia.com>

--- Silicon/NVIDIA/Drivers/TegraPwmDxe/TegraPwmDxe.c
+++ Silicon/NVIDIA/Drivers/TegraPwmDxe/TegraPwmDxe.c
@@ -2,7 +2,7 @@
 
   TegraPwmDxe Controller Driver
 
-  SPDX-FileCopyrightText: Copyright (c) 2020-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+  SPDX-FileCopyrightText: Copyright (c) 2020-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -37,6 +37,9 @@ NVIDIA_DEVICE_DISCOVERY_CONFIG  gDeviceDiscoverDriverConfig = {
 #define PWM_FAN_HIGH    0x81000000
 #define PWM_FAN_MED     0x80800000
 #define PWM_CLOCK_FREQ  19200000
+#define PWM_MAX         0x100
+#define PWM_OFFSET      16
+#define PWM_MASK        ((PWM_MAX - 1) << PWM_OFFSET)
 
 /**
   Callback that will be invoked at various phases of the driver initialization
@@ -71,6 +74,8 @@ DeviceDiscoveryNotify (
   UINT32                PwmLength;
   UINT32                FanPwmHandle;
   CONST CHAR8           *CompatArray[2];
+  UINT32                PwmPulseWidth;
+  UINT32                PwmCsrVal;
 
   switch (Phase) {
     case DeviceDiscoveryDriverBindingSupported:
@@ -123,10 +128,25 @@ DeviceDiscoveryNotify (
         return EFI_UNSUPPORTED;
       }
 
-      DeviceDiscoverySetClockFreq (ControllerHandle, "pwm", PWM_CLOCK_FREQ);
-      MmioWrite32 (BaseAddress, PWM_FAN_MED);
+      FanOffset      = -1;
+      CompatArray[0] = "pwm-fan";
+      CompatArray[1] = NULL;
+      Status         = DeviceTreeGetNextCompatibleNode (CompatArray, &FanOffset);
+      if (EFI_ERROR (Status)) {
+        return EFI_UNSUPPORTED;
+      }
 
-      return Status;
+      PwmCsrVal = PWM_FAN_MED;
+      Status    = DeviceTreeGetNodePropertyValue32 (FanOffset, "pulse-width", &PwmPulseWidth);
+      // Pulse width of high level is supposed to be in the range [0, 255].
+      if (!EFI_ERROR (Status) && (PwmPulseWidth < PWM_MAX)) {
+        PwmCsrVal = (PwmCsrVal & ~PWM_MASK) | (PwmPulseWidth << PWM_OFFSET);
+      }
+
+      DeviceDiscoverySetClockFreq (ControllerHandle, "pwm", PWM_CLOCK_FREQ);
+      MmioWrite32 (BaseAddress, PwmCsrVal);
+
+      return EFI_SUCCESS;
 
     default:
       return EFI_SUCCESS;
