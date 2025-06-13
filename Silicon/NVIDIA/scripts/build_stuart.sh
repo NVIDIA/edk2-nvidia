ea555ce6b446d9c195915acdad45177759e9abeb
Jeff Brasen <jbrasen@nvidia.com>
2025-06-13 13:55:04 -0700

    feat: Update build_stuart to use target argument
    Update the build_stuart.sh script to use the target argument to build
    the UEFI image.
    
    Use of the old environment variables is removed.
    
    Default behavior is changed from building both to just building debug if
    nothing is specified.
    
    Signed-off-by: Jeff Brasen <jbrasen@nvidia.com>
    Reviewed-by: Jake Garver <jake@nvidia.com>
    Tested-by: Jake Garver <jake@nvidia.com>

--- Silicon/NVIDIA/scripts/build_stuart.sh
+++ Silicon/NVIDIA/scripts/build_stuart.sh
@@ -4,8 +4,6 @@
 #
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 
-
-
 ###############################################################################
 # Build an NVIDIA UEFI image in the current workspace using the stuart build
 # system.  Assumes the workspace has been created using submodules and prepared
@@ -14,15 +12,9 @@
 # This script, along with others in this directory, serves as a working example
 # of building NVIDIA UEFI images.
 
-
-PLATFORM_BUILD=$1
-[ -z "${PLATFORM_BUILD}" ] && { echo "$0 <platform_build.py>"; exit 1; }
-
-
 # Set common environment variables.
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 . ${SCRIPT_DIR}/setenv_stuart.sh
-
 
 _msg "Activating Python virtual environment."
 find_venv_activate
@@ -34,12 +26,5 @@ if [[ "${UEFI_SKIP_UPDATE}" ]]; then
   STUART_BUILD_OPTIONS+=" --skip-verify"
 fi
 
-if [[ -z "${UEFI_RELEASE_ONLY}" ]]; then
-  _msg "Building DEBUG (${PLATFORM_BUILD})."
-  stuart_build -c $@ ${STUART_BUILD_OPTIONS} --target DEBUG
-fi
+stuart_build -c "$@" ${STUART_BUILD_OPTIONS}
 
-if [[ -z "${UEFI_DEBUG_ONLY}" ]]; then
-  _msg "Building RELEASE (${PLATFORM_BUILD})."
-  stuart_build -c $@ ${STUART_BUILD_OPTIONS} --target RELEASE
-fi
