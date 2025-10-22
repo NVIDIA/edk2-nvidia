11ca0fe1eaed6f955427948c9c8abd1b501d1a61
Jan Bobek <jbobek@nvidia.com>
2025-10-22 21:38:58 +0000

    feat(DisplayDeviceTreeHelperLib): introduce more DT update functions
    Introduce DisplayDeviceTreeUpdateMaxClockRates, which allows to specify
    maximum dispclk/hubclk rates in the DT.
    
    Introduce DisplayDeviceTreeUpdateIsoBandwidth, which allows to specify
    requested ISO bandwdith and memory clock floor in the DT.
    
    Signed-off-by: Jan Bobek <jbobek@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>
    Tested-by: Aron Wong <awong@nvidia.com>
    Reviewed-by: Aron Wong <awong@nvidia.com>

--- Silicon/NVIDIA/Include/Library/DisplayDeviceTreeHelperLib.h
+++ Silicon/NVIDIA/Include/Library/DisplayDeviceTreeHelperLib.h
@@ -1,6 +1,6 @@
 /** @file
 *
-*  Copyright (c) 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+*  SPDX-FileCopyrightText: Copyright (c) 2023-2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 *
 *  SPDX-License-Identifier: BSD-2-Clause-Patent
 *
@@ -31,6 +31,67 @@ UpdateDeviceTreeSimpleFramebufferInfo (
   IN CONST EFI_GRAPHICS_OUTPUT_MODE_INFORMATION  *ModeInfo,
   IN UINT64                                      FrameBufferBase,
   IN UINT64                                      FrameBufferSize
+  );
+
+/**
+  Update Device Tree display node with maximum dispclk/hubclk rates.
+
+  This function does not allocate any memory, hence it is safe to call
+  during ExitBootServices.
+
+  @param[in,opt] DeviceTree           Base of the Device Tree to update.
+  @param[in]     DisplayNodePath      Path to the display node.
+  @param[in]     MaxDispClkRateKhz    Maximum dispclk rates in kHz.
+  @param[in]     MaxDispClkRateCount  Number of maximum dispclk rates.
+  @param[in]     MaxHubClkRateKhz     Maximum hubclk rates in kHz.
+  @param[in]     MaxHubClkRateCount   Number of maximum hubclk rates.
+
+  @retval EFI_SUCCESS            Node successfully updated.
+  @retval EFI_INVALID_PARAMETER  DisplayNodePath is NULL.
+  @retval EFI_INVALID_PARAMETER  MaxDispClkRateCount is non-zero, but
+                                 MaxDispClkRateKhz is NULL.
+  @retval EFI_INVALID_PARAMETER  MaxHubClkRateCount is non-zero, but
+                                 MaxHubClkRateKhz is NULL.
+  @retval EFI_OUT_OF_RESOURCES   MaxDispClkRateCount is too large.
+  @retval EFI_OUT_OF_RESOURCES   MaxHubClkRateCount is too large.
+  @retval EFI_NOT_FOUND          Node specified by DevicePath not found.
+  @retval EFI_DEVICE_ERROR       Other errors.
+*/
+EFI_STATUS
+EFIAPI
+DisplayDeviceTreeUpdateMaxClockRates (
+  IN VOID          *DeviceTree  OPTIONAL,
+  IN CONST CHAR8   *DisplayNodePath,
+  IN CONST UINT32  *MaxDispClkRateKhz,
+  IN UINTN         MaxDispClkRateCount,
+  IN CONST UINT32  *MaxHubClkRateKhz,
+  IN UINTN         MaxHubClkRateCount
+  );
+
+/**
+  Update Device Tree display node with allocated ISO bandwidth and
+  memory clock floor.
+
+  This function does not allocate any memory, hence it is safe to call
+  during ExitBootServices.
+
+  @param[in,opt] DeviceTree                 Base of the Device Tree to update.
+  @param[in]     DisplayNodePath            Path to the display node.
+  @param[in]     IsoBandwidthKbytesPerSec   Requested ISO bandwidth.
+  @param[in]     MemclockFloorKbytesPerSec  Requested memory clock floor.
+
+  @retval EFI_SUCCESS            Node successfully updated.
+  @retval EFI_INVALID_PARAMETER  DisplayNodePath is NULL.
+  @retval EFI_NOT_FOUND          Node specified by DevicePath not found.
+  @retval EFI_DEVICE_ERROR       Other errors.
+*/
+EFI_STATUS
+EFIAPI
+DisplayDeviceTreeUpdateIsoBandwidth (
+  IN VOID         *DeviceTree  OPTIONAL,
+  IN CONST CHAR8  *DisplayNodePath,
+  IN UINT32       IsoBandwidthKbytesPerSec,
+  IN UINT32       MemclockFloorKbytesPerSec
   );
 
 #endif //__DISPLAY_DEVICE_TREE_HELPER_LIB_H__
