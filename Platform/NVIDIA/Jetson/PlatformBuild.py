942f38f83b686174e6a962cbf2c7ba3b65ebe425
Jake Garver <jake@nvidia.com>
2025-11-05 08:24:50 -0800

    chore: Update release version to 202210.5.3
    Signed-off-by: Jake Garver <jake@nvidia.com>

--- Platform/NVIDIA/Jetson/PlatformBuild.py
+++ Platform/NVIDIA/Jetson/PlatformBuild.py
@@ -20,7 +20,7 @@ class JetsonSettingsManager(NVIDIASettingsManager):
         return super().GetActiveScopes() + ["jetson"]
 
     def GetFirmwareVersionBase(self):
-        return "202210.5.2"
+        return "202210.5.3"
 
     def GetFirmwareVolume(self):
         return "FV/UEFI_NS.Fv"
