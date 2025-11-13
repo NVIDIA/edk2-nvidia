e132a05680a7c5c5cf1bd70dcc6b1fe3c6be6a45
Bob Morgan <bobm@nvidia.com>
2025-11-13 14:12:41 -0800

    fix: Add dependency for Android FmpDxe
    Add gNVIDIAAndroidFmpInitCompleteProtocolGuid protocol
    to be installed by AndroidFmpInitDxe upon completion.
    
    Add depex library to Android build of FmpDxe with
    dependency on gNVIDIAAndroidFmpInitCompleteProtocolGuid.
    
    Signed-off-by: Bob Morgan <bobm@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>

---
+++ Silicon/NVIDIA/Library/DepexLib/DepexLib.c
@@ -0,0 +1,9 @@
+/** @file
+
+  Empty library to add dependency to a driver build.
+
+  SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
