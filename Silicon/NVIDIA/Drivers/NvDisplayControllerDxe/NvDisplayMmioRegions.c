f5930bfa6a3e11330b7e43d0da57cae470b44b6b
Jan Bobek <jbobek@nvidia.com>
2025-12-02 00:58:11 +0000

    feat(NvDisplayControllerDxe): parse framebuffer carveout header if present
    Parse the framebuffer carveout header if one is present, and use the
    early framebuffer subcarveout in that case. This enables sharing the
    framebuffer carveout with DCE DTB. Ensure the final carveout is aligned
    properly.
    
    Signed-off-by: Jan Bobek <jbobek@nvidia.com>
    Reviewed-by: Jeff Brasen <jbrasen@nvidia.com>

--- Silicon/NVIDIA/Drivers/NvDisplayControllerDxe/NvDisplayMmioRegions.c
+++ Silicon/NVIDIA/Drivers/NvDisplayControllerDxe/NvDisplayMmioRegions.c
@@ -2,7 +2,7 @@
 
   NV Display Controller Driver - MMIO regions
 
-  SPDX-FileCopyrightText: Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+  SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 
   SPDX-License-Identifier: BSD-2-Clause-Patent
 
@@ -24,6 +24,43 @@
 #include "NvDisplay.h"
 
 #pragma pack (push, 1)
+typedef struct {
+  UINT32    Offset;
+  UINT32    Size;
+} NV_FB_SUB_CARVEOUT_INFO;
+
+typedef struct {
+  UINT32                     Signature;
+  UINT32                     Version;
+  UINT8                      Digest[64];
+  UINT32                     SubCarveoutCount;
+  NV_FB_SUB_CARVEOUT_INFO    SubCarveouts[8];
+  UINT8                      Reserved[116];
+} NV_FB_CARVEOUT_HEADER;
+
+///
+/// Framebuffer carveout header signature.
+///
+#define NV_FB_CARVEOUT_HEADER_SIGNATURE  SIGNATURE_32 ('D', 'C', 'D', 'T')
+///
+/// Framebuffer carveout header version.
+///
+#define NV_FB_CARVEOUT_HEADER_VERSION  1
+
+///
+/// Index of the early framebuffer sub-carveout.
+///
+#define NV_FB_SUB_CARVEOUT_INDEX_EARLY_FB  0
+///
+/// Index of the DCE DTB sub-carveout.
+///
+#define NV_FB_SUB_CARVEOUT_INDEX_DCE_DTB  1
+
+STATIC_ASSERT (
+  sizeof (NV_FB_CARVEOUT_HEADER) == 256,
+  "NV_FB_CARVEOUT_HEADER is expected to be exactly 256 bytes long."
+  );
+
 typedef struct {
   EFI_ACPI_ADDRESS_SPACE_DESCRIPTOR    Registers;
   EFI_ACPI_ADDRESS_SPACE_DESCRIPTOR    Framebuffer;
@@ -137,13 +174,76 @@ InitializeAcpiDtRegistersDescriptor (
 }
 
 /**
+  Retrieve start/end address of a framebuffer sub-carveout.
+
+  @param[in]  FbBase        Base of the framebuffer carveout.
+  @param[in]  FbSize        Size of the framebuffer carveout.
+  @param[in]  Index         Index of the sub-carveout.
+  @param[out] StartAddress  Start address of the sub-carveout.
+  @param[out] EndAddress    End address of the sub-carveout.
+
+  @retval EFI_SUCCESS            Sub-carveout successfully retrieved.
+  @retval EFI_UNSUPPORTED        Framebuffer carveout too small for header.
+  @retval EFI_UNSUPPORTED        Framebuffer carveout header signature invalid.
+  @retval EFI_NOT_FOUND          Framebuffer carveout header version unknown.
+  @retval EFI_NOT_FOUND          Specified sub-carveout does not exist.
+  @retval EFI_INVALID_PARAMETER  Number of sub-carveouts is too large.
+  @retval EFI_INVALID_PARAMETER  The sub-carveout is not fully contained within
+                                 the framebuffer carveout.
+*/
+STATIC
+EFI_STATUS
+GetFbSubCarveout (
+  IN  CONST EFI_PHYSICAL_ADDRESS   FbBase,
+  IN  CONST UINTN                  FbSize,
+  IN  CONST UINTN                  Index,
+  OUT EFI_PHYSICAL_ADDRESS *CONST  StartAddress,
+  OUT EFI_PHYSICAL_ADDRESS *CONST  EndAddress
+  )
+{
+  NV_FB_CARVEOUT_HEADER    *Hdr;
+  NV_FB_SUB_CARVEOUT_INFO  *SubCarveout;
+
+  if (!(FbSize >= sizeof (*Hdr))) {
+    return EFI_UNSUPPORTED;
+  }
+
+  Hdr = (NV_FB_CARVEOUT_HEADER *)FbBase;
+  if (Hdr->Signature != NV_FB_CARVEOUT_HEADER_SIGNATURE) {
+    return EFI_UNSUPPORTED;
+  }
+
+  if (Hdr->Version != NV_FB_CARVEOUT_HEADER_VERSION) {
+    return EFI_NOT_FOUND;
+  }
+
+  if (!(Hdr->SubCarveoutCount <= ARRAY_SIZE (Hdr->SubCarveouts))) {
+    return EFI_INVALID_PARAMETER;
+  } else if (!(Index < Hdr->SubCarveoutCount)) {
+    return EFI_NOT_FOUND;
+  }
+
+  SubCarveout = &Hdr->SubCarveouts[Index];
+  if (!(  (SubCarveout->Offset <= FbSize)
+       && (SubCarveout->Size <= FbSize - SubCarveout->Offset)))
+  {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  *StartAddress = FbBase + SubCarveout->Offset;
+  *EndAddress   = FbBase + SubCarveout->Offset + SubCarveout->Size;
+  return EFI_SUCCESS;
+}
+
+/**
   Retrieves base and size of the framebuffer region.
 
   @param[out] Base  Base of the framebuffer region.
   @param[out] Size  Size of the framebuffer region.
 
-  @retval EFI_SUCCESS    Region details retrieved successfully.
-  @retval EFI_NOT_FOUND  The framebuffer region was not found.
+  @retval EFI_SUCCESS       Region details retrieved successfully.
+  @retval EFI_NOT_FOUND     The framebuffer region was not found.
+  @retval EFI_DEVICE_ERROR  The framebuffer region was invalid.
 */
 EFI_STATUS
 NvDisplayGetFramebufferRegion (
@@ -151,9 +251,12 @@ NvDisplayGetFramebufferRegion (
   OUT UINTN                *CONST  Size
   )
 {
+  EFI_STATUS                    Status;
   VOID                          *Hob;
   TEGRA_PLATFORM_RESOURCE_INFO  *PlatformResourceInfo;
   TEGRA_BASE_AND_SIZE_INFO      *FbInfo;
+  EFI_PHYSICAL_ADDRESS          StartAddress, EndAddress;
+  CONST UINTN                   Alignment = RUNTIME_PAGE_ALLOCATION_GRANULARITY;
 
   Hob = GetFirstGuidHob (&gNVIDIAPlatformResourceDataGuid);
   if ((Hob == NULL) || (GET_GUID_HOB_DATA_SIZE (Hob) != sizeof (*PlatformResourceInfo))) {
@@ -177,8 +280,51 @@ NvDisplayGetFramebufferRegion (
     return EFI_NOT_FOUND;
   }
 
-  *Base = FbInfo->Base;
-  *Size = FbInfo->Size;
+  Status = GetFbSubCarveout (
+             FbInfo->Base,
+             FbInfo->Size,
+             NV_FB_SUB_CARVEOUT_INDEX_EARLY_FB,
+             &StartAddress,
+             &EndAddress
+             );
+  if (Status == EFI_UNSUPPORTED) {
+    /* Sub-carveouts not supported, fall back to using the entire
+       framebuffer region. */
+    StartAddress = (EFI_PHYSICAL_ADDRESS)FbInfo->Base;
+    EndAddress   = StartAddress + FbInfo->Size;
+  } else if (Status == EFI_NOT_FOUND) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "%a: no framebuffer sub-carveout present\r\n",
+      __FUNCTION__
+      ));
+    return EFI_NOT_FOUND;
+  } else if (EFI_ERROR (Status)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "%a: failed to retrieve framebuffer sub-carveout: %r\r\n",
+      __FUNCTION__,
+      Status
+      ));
+    return EFI_DEVICE_ERROR;
+  }
+
+  StartAddress = ALIGN_VALUE (StartAddress, Alignment);
+  EndAddress   = EndAddress & ~(Alignment - 1);
+
+  if (!(StartAddress < EndAddress)) {
+    DEBUG ((
+      DEBUG_ERROR,
+      "%a: invalid framebuffer region: 0x%016lx:0x%016lx\r\n",
+      __FUNCTION__,
+      StartAddress,
+      EndAddress
+      ));
+    return EFI_DEVICE_ERROR;
+  }
+
+  *Base = StartAddress;
+  *Size = EndAddress - StartAddress;
 
   return EFI_SUCCESS;
 }
